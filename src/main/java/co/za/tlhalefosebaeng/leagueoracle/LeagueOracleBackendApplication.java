34ad01fc5f3fcde3cc970bd2d260faf6e1c5e208
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-24 19:41:18 +0200

    First commit

---
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/LeagueOracleBackendApplication.java
@@ -0,0 +1,13 @@
+package co.za.tlhalefosebaeng.leagueoracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueOracleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueOracleBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/za/tlhalefosebaeng/leagueoracle/LeagueOracleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package co.za.tlhalefosebaeng.leagueoracle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueOracleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
