438706cc5e7fbc16c2fcc67f52a1b20e73d5e796
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-25 08:04:44 +0200

    Create league model

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/model/League.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/model/League.java
@@ -1,4 +1,22 @@
 package co.za.tlhalefosebaeng.leagueoracle.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class League {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
 }
