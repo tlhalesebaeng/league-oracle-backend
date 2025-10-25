31f193c14de63c50269471d458d8a1bcc27fca44
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-25 09:04:33 +0200

    Catch the HttpNotReadableException with the global exception handler

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/exceptions/GlobalExceptionHandler.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/exceptions/GlobalExceptionHandler.java
@@ -1,4 +1,16 @@
 package co.za.tlhalefosebaeng.leagueoracle.exceptions;
 
+import co.za.tlhalefosebaeng.leagueoracle.response.MessageResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler({ HttpMessageNotReadableException.class })
+    public ResponseEntity<MessageResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MessageResponse("Request body not readable"));
+    }
 }
