543c59098a11a99e3700b52b89bc0b0c2a032f8a
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-25 08:42:52 +0200

    Add league response dto name field

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/league/LeagueResponse.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/league/LeagueResponse.java
@@ -1,4 +1,10 @@
 package co.za.tlhalefosebaeng.leagueoracle.dto.league;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class LeagueResponse {
+    private String name;
 }
