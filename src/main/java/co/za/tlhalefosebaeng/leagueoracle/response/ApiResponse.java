5dfd29382cd55582da255bd0d8768a85a53db7a5
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-25 08:21:57 +0200

    Include an all arguments constructor

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/response/ApiResponse.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/response/ApiResponse.java
@@ -1,8 +1,10 @@
 package co.za.tlhalefosebaeng.leagueoracle.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ApiResponse {
     private String message;
     private Object data;
