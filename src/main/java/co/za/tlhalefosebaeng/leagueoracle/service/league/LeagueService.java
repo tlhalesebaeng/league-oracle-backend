5e04e31e1458fe25411575e0f8a0c045801ed1ed
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-25 08:06:09 +0200

    Prepare to create a new league

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/league/LeagueService.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/league/LeagueService.java
@@ -1,4 +1,12 @@
-package co.za.tlhalefosebaeng.leagueoracle.service;
+package co.za.tlhalefosebaeng.leagueoracle.service.league;
 
-public class LeagueService {
+import co.za.tlhalefosebaeng.leagueoracle.model.League;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LeagueService implements LeagueServiceInterface {
+    @Override
+    public League createLeague(League league) {
+        return null;
+    }
 }
--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/league/LeagueServiceInterface.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/league/LeagueServiceInterface.java
@@ -1,4 +1,7 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.league;
 
+import co.za.tlhalefosebaeng.leagueoracle.model.League;
+
 public interface LeagueServiceInterface {
+    League createLeague(League league);
 }
