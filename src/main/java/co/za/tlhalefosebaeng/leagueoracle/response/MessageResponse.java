8b7b7e2baa0ac5e518489e01688fc95e32ec5f55
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-25 08:58:45 +0200

    Add message response message property

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/response/MessageResponse.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/response/MessageResponse.java
@@ -1,4 +1,10 @@
 package co.za.tlhalefosebaeng.leagueoracle.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class MessageResponse {
+    private String message;
 }
