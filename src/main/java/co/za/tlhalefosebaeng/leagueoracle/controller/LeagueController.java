26e1fe37887812d55a504b1329fe7f9cfae22fd9
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-25 08:06:41 +0200

    Accept a post request for creating a new league

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/controller/LeagueController.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/controller/LeagueController.java
@@ -1,4 +1,22 @@
 package co.za.tlhalefosebaeng.leagueoracle.controller;
 
+import co.za.tlhalefosebaeng.leagueoracle.model.League;
+import co.za.tlhalefosebaeng.leagueoracle.service.league.LeagueServiceInterface;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("${api.endpoint.prefix}/leagues")
 public class LeagueController {
+    public final LeagueServiceInterface leagueService;
+
+    @PostMapping("")
+    public ResponseEntity<League> createLeague() {
+        League league = leagueService.createLeague(null);
+        return ResponseEntity.ok(league);
+    }
 }
