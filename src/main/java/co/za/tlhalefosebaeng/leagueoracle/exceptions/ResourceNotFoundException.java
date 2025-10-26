8deb5b8a61b7ebae9b257680a3352e5435fb0c5f
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-26 14:38:05 +0200

    Define a constructor that accepts a message and calls the parent class with the message

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/exceptions/ResourceNotFoundException.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/exceptions/ResourceNotFoundException.java
@@ -1,4 +1,7 @@
 package co.za.tlhalefosebaeng.leagueoracle.exceptions;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
