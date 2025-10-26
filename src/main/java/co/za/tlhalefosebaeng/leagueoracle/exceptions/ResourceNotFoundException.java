223a5dbbd03cf1db0a45304a6f3d4586456eae4b
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-26 14:56:03 +0200

    Add a status code property and assign it using the constructor

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/exceptions/ResourceNotFoundException.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/exceptions/ResourceNotFoundException.java
@@ -1,7 +1,16 @@
 package co.za.tlhalefosebaeng.leagueoracle.exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
 public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
+    private HttpStatus code;
+    public ResourceNotFoundException(HttpStatus code, String message) {
         super(message);
+        this.code = code;
     }
 }
