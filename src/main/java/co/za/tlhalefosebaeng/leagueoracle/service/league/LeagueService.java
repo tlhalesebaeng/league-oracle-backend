8866f87f6029a49dd811dc446e4ace88394713e5
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-26 11:31:49 +0200

    Persist the league to the database: Basic

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/league/LeagueService.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/league/LeagueService.java
@@ -2,12 +2,19 @@ package co.za.tlhalefosebaeng.leagueoracle.service.league;
 
 import co.za.tlhalefosebaeng.leagueoracle.dto.league.LeagueRequest;
 import co.za.tlhalefosebaeng.leagueoracle.model.League;
+import co.za.tlhalefosebaeng.leagueoracle.repository.LeagueRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class LeagueService implements LeagueServiceInterface {
+    private final LeagueRepository leagueRepo;
+
     @Override
     public League createLeague(LeagueRequest league) {
-        return null;
+        League pendingLeague = new League();
+        pendingLeague.setName(league.getName());
+        return leagueRepo.save(pendingLeague);
     }
 }
