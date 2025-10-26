0e0719b0c31dbd1b27a39fba493d869e81021ef7
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-26 11:28:59 +0200

    Add a unique and non nullable properties to the league name field

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/model/League.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/model/League.java
@@ -1,9 +1,6 @@
 package co.za.tlhalefosebaeng.leagueoracle.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,5 +15,7 @@ public class League {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true, nullable = false)
     private String name;
 }
