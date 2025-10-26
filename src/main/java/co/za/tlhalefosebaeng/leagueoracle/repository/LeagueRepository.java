a197055919f7d1757ec4bd1fe3b3b7e47a664c26
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-26 11:29:58 +0200

    Extend JpaRepository interface

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/repository/LeagueRepository.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/repository/LeagueRepository.java
@@ -1,4 +1,7 @@
 package co.za.tlhalefosebaeng.leagueoracle.repository;
 
-public class LeagueRepository {
+import co.za.tlhalefosebaeng.leagueoracle.model.League;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LeagueRepository extends JpaRepository<League, Long> {
 }
