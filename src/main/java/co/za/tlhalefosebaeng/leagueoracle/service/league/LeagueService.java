0cadb82fb9cdda23d44fd37627cb2864204cb465
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-26 15:14:31 +0200

    Add a method to query the database for all leagues

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/league/LeagueService.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/league/LeagueService.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -26,5 +27,10 @@ public class LeagueService implements LeagueServiceInterface {
     public League getLeague(Long leagueId) {
         Optional<League> league = leagueRepo.findById(leagueId);
         return league.orElseThrow(() -> new ResourceNotFoundException(HttpStatus.BAD_REQUEST, "League not found! Please check league ID and try again."));
+    }
+
+    @Override
+    public List<League> getAllLeagues() {
+        return leagueRepo.findAll();
     }
 }
--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/league/LeagueServiceInterface.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/league/LeagueServiceInterface.java
@@ -3,7 +3,10 @@ package co.za.tlhalefosebaeng.leagueoracle.service.league;
 import co.za.tlhalefosebaeng.leagueoracle.dto.league.LeagueRequest;
 import co.za.tlhalefosebaeng.leagueoracle.model.League;
 
+import java.util.List;
+
 public interface LeagueServiceInterface {
     League createLeague(LeagueRequest league);
     League getLeague(Long leagueId);
+    List<League> getAllLeagues();
 }
