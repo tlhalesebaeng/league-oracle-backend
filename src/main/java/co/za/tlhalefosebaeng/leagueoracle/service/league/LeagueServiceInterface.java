3d10f50484669426c3817b09cc59eac528667820
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-27 21:14:46 +0200

    Return the league response dto from create league method

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/league/LeagueServiceInterface.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/league/LeagueServiceInterface.java
@@ -1,12 +1,13 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.league;
 
 import co.za.tlhalefosebaeng.leagueoracle.dto.league.LeagueRequest;
+import co.za.tlhalefosebaeng.leagueoracle.dto.league.LeagueResponse;
 import co.za.tlhalefosebaeng.leagueoracle.model.League;
 
 import java.util.List;
 
 public interface LeagueServiceInterface {
-    League createLeague(LeagueRequest league);
+    LeagueResponse createLeague(LeagueRequest league);
     League getLeague(Long leagueId);
     List<League> getAllLeagues(String name);
     League updateLeague(Long leagueId, LeagueRequest league);
