03bab408c5695a7a640c4fe8bb66ac7ca65ac153
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-27 21:25:44 +0200

    Return the league response dto from the get all leagues method

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/league/LeagueServiceInterface.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/league/LeagueServiceInterface.java
@@ -9,7 +9,7 @@ import java.util.List;
 public interface LeagueServiceInterface {
     LeagueResponse createLeague(LeagueRequest league);
     League getLeague(Long leagueId);
-    List<League> getAllLeagues(String name);
+    List<LeagueResponse> getAllLeagues(String name);
     League updateLeague(Long leagueId, LeagueRequest league);
     void deleteLeague(Long leagueId);
 }
