0fe57b2455e2b66984b2ff6c0a066e6f40ead9b1
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-27 11:49:35 +0200

    Define league properties

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/model/Team.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/model/Team.java
@@ -1,4 +1,12 @@
 package co.za.tlhalefosebaeng.leagueoracle.model;
 
 public class Team {
+    private Long id;
+    private String name;
+    private Integer wins;
+    private Integer draws;
+    private Integer loses;
+    private Integer goalsForward;
+    private Integer goalsAgainst;
+    private League league;
 }
