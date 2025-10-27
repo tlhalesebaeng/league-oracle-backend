4125d89e14d6f07d288bb1a1b2d92c519b848313
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-27 16:24:32 +0200

    Include teams on the league response

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/league/LeagueResponse.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/league/LeagueResponse.java
@@ -1,10 +1,16 @@
 package co.za.tlhalefosebaeng.leagueoracle.dto.league;
 
+import co.za.tlhalefosebaeng.leagueoracle.dto.team.TeamResponse;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 public class LeagueResponse {
+    // We don't need any validation on fields of this class since we are the one managing it's objects
     private String name;
+
+    private List<TeamResponse> teams;
 }
