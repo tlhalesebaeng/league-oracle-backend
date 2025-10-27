bb7f637fda10b596384afefe661ab8d2ff89b387
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-27 16:23:57 +0200

    Add team response dto fields and methods

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/team/TeamResponse.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/team/TeamResponse.java
@@ -1,4 +1,19 @@
 package co.za.tlhalefosebaeng.leagueoracle.dto.team;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class TeamResponse {
+    private Long id;
+    private String name;
+    private int wins;
+    private int draws;
+    private int loses;
+    private int goalsForward;
+    private int goalsAgainst;
+    private int playedGames;
+    private int points;
+    private int goalDifference;
 }
