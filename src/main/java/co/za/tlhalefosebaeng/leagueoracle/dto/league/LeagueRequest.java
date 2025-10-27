d85eb774d7b15a4dfa7d6523043e3e799f6f8783
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-27 15:42:13 +0200

    Add minimum and maximum contraints on the teams

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/league/LeagueRequest.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/league/LeagueRequest.java
@@ -2,6 +2,7 @@ package co.za.tlhalefosebaeng.leagueoracle.dto.league;
 
 import co.za.tlhalefosebaeng.leagueoracle.model.Team;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -13,5 +14,7 @@ public class LeagueRequest {
     @NotBlank(message = "Name required! Please provide a league name")
     private String name;
 
+    @Size(min = 2, message = "A league should have at least two (2) teams")
+    @Size(max = 10, message = "A league can have a max of ten (10) teams")
     private List<Team> teams;
 }
