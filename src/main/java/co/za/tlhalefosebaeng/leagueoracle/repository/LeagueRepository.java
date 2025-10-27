52179071eb359315967d57eef7f3a33951f138be
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-27 11:26:01 +0200

    Create a custom JPA finder method to get all the leagues that has a name subword in their league name

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/repository/LeagueRepository.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/repository/LeagueRepository.java
@@ -2,6 +2,12 @@ package co.za.tlhalefosebaeng.leagueoracle.repository;
 
 import co.za.tlhalefosebaeng.leagueoracle.model.League;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface LeagueRepository extends JpaRepository<League, Long> {
+    @Query("SELECT e FROM League e WHERE e.name LIKE %:name%")
+    List<League> findAllByLeagueName(@Param("name") String name);
 }
