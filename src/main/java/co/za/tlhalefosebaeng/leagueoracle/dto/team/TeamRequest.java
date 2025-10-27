a138f735ff316b805c361b69343c97ad59279a7f
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-27 22:11:35 +0200

    Add data methods and the all args constructor

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/team/TeamRequest.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/team/TeamRequest.java
@@ -1,5 +1,10 @@
 package co.za.tlhalefosebaeng.leagueoracle.dto.team;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class TeamRequest {
     private String name;
 }
