914e36f44ac74ef2ca34e7820c9194b0c0e85289
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-27 15:05:18 +0200

    Make the team class an entity and link it to the league

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/model/Team.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/model/Team.java
@@ -1,12 +1,31 @@
 package co.za.tlhalefosebaeng.leagueoracle.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private Integer wins;
     private Integer draws;
     private Integer loses;
     private Integer goalsForward;
     private Integer goalsAgainst;
+
+    @ManyToOne
+    @JoinColumn(name = "league_id")
+    @JsonIgnore
     private League league;
 }
