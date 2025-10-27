544efe3688789596c186264c922ee4e76eed4488
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-27 21:13:55 +0200

    Add a no args constructor to help us write readable code

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/league/LeagueResponse.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/league/LeagueResponse.java
@@ -3,11 +3,13 @@ package co.za.tlhalefosebaeng.leagueoracle.dto.league;
 import co.za.tlhalefosebaeng.leagueoracle.dto.team.TeamResponse;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class LeagueResponse {
     // We don't need any validation on fields of this class since we are the one managing it's objects
     private String name;
