13bd10e6453237916d4d1d7b6057d0d823afdb9f
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-28 11:32:23 +0200

    Add the update team request dto fields

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/team/UpdateTeamRequest.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/team/UpdateTeamRequest.java
@@ -1,4 +1,10 @@
 package co.za.tlhalefosebaeng.leagueoracle.dto.team;
 
 public class UpdateTeamRequest {
+    private String name;
+    private int wins;
+    private int draws;
+    private int loses;
+    private int goalsForward;
+    private int goalsAgainst;
 }
