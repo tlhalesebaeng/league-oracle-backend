48c2b48c2c78b05b62b3d4d7928b4fb72cb8437a
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-28 11:28:26 +0200

    Add a not blank constraint on the league name

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/team/TeamRequest.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/team/TeamRequest.java
@@ -1,10 +1,12 @@
 package co.za.tlhalefosebaeng.leagueoracle.dto.team;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class TeamRequest {
+    @NotBlank(message = "Team name required! Please provide a team name")
     private String name;
 }
