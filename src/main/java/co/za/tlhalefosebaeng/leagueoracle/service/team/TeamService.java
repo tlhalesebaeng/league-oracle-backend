4b498a399a1958b841e126e0a953a9b6c87056f7
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-28 11:58:28 +0200

    Remove the add team to league method from the interface and the interface's implementation

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/team/TeamService.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/team/TeamService.java
@@ -1,4 +1,31 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.team;
 
-public class TeamService {
+import co.za.tlhalefosebaeng.leagueoracle.dto.team.AddTeamRequest;
+import co.za.tlhalefosebaeng.leagueoracle.model.League;
+import co.za.tlhalefosebaeng.leagueoracle.model.Team;
+import co.za.tlhalefosebaeng.leagueoracle.repository.LeagueRepository;
+import co.za.tlhalefosebaeng.leagueoracle.service.league.LeagueServiceInterface;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TeamService implements TeamServiceInterface{
+    private final LeagueServiceInterface leagueService;
+    private final LeagueRepository leagueRepo;
+
+    @Override
+    public League addTeamToLeague(Long leagueId, AddTeamRequest team) {
+        // Get the league from the database
+        League league = leagueService.getLeague(leagueId);
+
+        // Set the necessary team properties
+        Team newTeam = new Team();
+        newTeam.setLeague(league);
+        newTeam.setName(team.getName());
+
+        // Add the team to the list of teams and return the newly saved league
+        league.getTeams().add(newTeam);
+        return leagueRepo.save(league);
+    }
 }
--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/team/TeamServiceInterface.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/team/TeamServiceInterface.java
@@ -1,4 +1,8 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.team;
 
+import co.za.tlhalefosebaeng.leagueoracle.dto.team.AddTeamRequest;
+import co.za.tlhalefosebaeng.leagueoracle.model.League;
+
 public interface TeamServiceInterface {
+    League addTeamToLeague(Long leagueId, AddTeamRequest team);
 }
