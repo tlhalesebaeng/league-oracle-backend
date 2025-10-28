282bae0c54c2b50f13d1652a49ad7094db7854ea
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-28 15:05:57 +0200

    Add necessary annotations

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/model/Fixture.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/model/Fixture.java
@@ -1,8 +1,24 @@
 package co.za.tlhalefosebaeng.leagueoracle.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Fixture {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Date date;
     private String venue;
