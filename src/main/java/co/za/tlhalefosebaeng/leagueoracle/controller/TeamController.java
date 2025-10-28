65c9e3649e6ba61cdebd99654a2f75320183be75
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-28 12:01:21 +0200

    Expose a new endpoint to add a team to the league

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/controller/TeamController.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/controller/TeamController.java
@@ -1,11 +1,27 @@
 package co.za.tlhalefosebaeng.leagueoracle.controller;
 
+import co.za.tlhalefosebaeng.leagueoracle.dto.team.AddTeamRequest;
+import co.za.tlhalefosebaeng.leagueoracle.model.League;
+import co.za.tlhalefosebaeng.leagueoracle.response.ApiResponse;
+import co.za.tlhalefosebaeng.leagueoracle.service.league.LeagueServiceInterface;
+import co.za.tlhalefosebaeng.leagueoracle.service.team.TeamServiceInterface;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("${api.endpoint.prefix}/teams")
 public class TeamController {
+    private final TeamServiceInterface teamService;
+    private final LeagueServiceInterface leagueService;
+
+    @PostMapping("")
+    public ResponseEntity<ApiResponse> addTeamToLeague(@RequestParam Long leagueId, @RequestBody AddTeamRequest team){
+        // Add the team to the league corresponding to this leagueId
+        League league = teamService.addTeamToLeague(leagueId, team);
+
+        // Convert the league to a league response dto and return it
+        return ResponseEntity.ok(new ApiResponse("success", leagueService.convertLeagueToDto(league)));
+    }
 }
