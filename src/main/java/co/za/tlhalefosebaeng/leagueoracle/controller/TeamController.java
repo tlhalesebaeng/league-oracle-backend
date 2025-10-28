d24ce1b1ba2888af9568ae0ff478454a3198082a
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-28 11:40:11 +0200

    Add necessary annotations

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/controller/TeamController.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/controller/TeamController.java
@@ -1,4 +1,11 @@
 package co.za.tlhalefosebaeng.leagueoracle.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("${api.endpoint.prefix}/teams")
 public class TeamController {
 }
