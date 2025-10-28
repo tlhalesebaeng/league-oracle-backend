ebb5d8a789df392261bffec8aa446f73d122b626
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-28 15:04:45 +0200

    Define fixture model property

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/model/Fixture.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/model/Fixture.java
@@ -1,4 +1,10 @@
 package co.za.tlhalefosebaeng.leagueoracle.model;
 
+import java.util.Date;
+
 public class Fixture {
+    private Long id;
+    private Date date;
+    private String venue;
+    private String field;
 }
