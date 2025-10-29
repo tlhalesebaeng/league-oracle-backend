0ada3b93ee1371369d6e7c9f657edd80b1898b22
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-29 22:37:27 +0200

    Define a method to convert a given fixture to a fixture dto

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/fixture/FixtureServiceInterface.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/fixture/FixtureServiceInterface.java
@@ -1,9 +1,11 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.fixture;
 
+import co.za.tlhalefosebaeng.leagueoracle.dto.fixture.FixtureResponse;
 import co.za.tlhalefosebaeng.leagueoracle.model.Fixture;
 
 import java.util.List;
 
 public interface FixtureServiceInterface {
     List<Fixture> generateLeagueFixtures(Long leagueId);
+    FixtureResponse convertFixtureToDto(Fixture fixture);
 }
