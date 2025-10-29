ec97ef47b90eaf6a5a6ca5b5c2e92a6fcfcb7f87
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-29 14:23:49 +0200

    Define a method to generate fixtures and return them

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/fixture/FixtureServiceInterface.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/fixture/FixtureServiceInterface.java
@@ -1,4 +1,9 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.fixture;
 
+import co.za.tlhalefosebaeng.leagueoracle.model.Fixture;
+
+import java.util.List;
+
 public interface FixtureServiceInterface {
+    List<Fixture> generateLeagueFixtures(Long leagueId);
 }
