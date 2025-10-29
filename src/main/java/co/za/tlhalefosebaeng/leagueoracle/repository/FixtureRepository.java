e0ab8de3d2ca79a11fb169ab98e0bd65da9de157
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-29 22:54:17 +0200

    Use jpa finder methods definition to define a method to find all fixtures by league id

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/repository/FixtureRepository.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/repository/FixtureRepository.java
@@ -3,5 +3,8 @@ package co.za.tlhalefosebaeng.leagueoracle.repository;
 import co.za.tlhalefosebaeng.leagueoracle.model.Fixture;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FixtureRepository extends JpaRepository<Fixture, Long> {
+    List<Fixture> findAllByLeagueId(Long leagueId);
 }
