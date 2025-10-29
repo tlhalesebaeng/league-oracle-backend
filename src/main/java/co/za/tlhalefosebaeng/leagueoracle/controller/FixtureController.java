72a27fc27264a2662850d1f8ee45327c2ecabc8b
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-29 22:39:13 +0200

    Conver the leagues on the response to league response dtos

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/controller/FixtureController.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/controller/FixtureController.java
@@ -1,5 +1,6 @@
 package co.za.tlhalefosebaeng.leagueoracle.controller;
 
+import co.za.tlhalefosebaeng.leagueoracle.dto.fixture.FixtureResponse;
 import co.za.tlhalefosebaeng.leagueoracle.model.Fixture;
 import co.za.tlhalefosebaeng.leagueoracle.response.ApiResponse;
 import co.za.tlhalefosebaeng.leagueoracle.service.fixture.FixtureServiceInterface;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -22,6 +24,13 @@ public class FixtureController {
     @PostMapping("")
     public ResponseEntity<ApiResponse> createLeagueFixtures(@RequestParam Long leagueId) {
         List<Fixture> fixtures = fixtureService.generateLeagueFixtures(leagueId);
-        return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse("success", fixtures));
+
+        // Convert the fixtures to fixture response dtos
+        List<FixtureResponse> fixtureResponses = new ArrayList<>();
+        for(Fixture fixture : fixtures) {
+            fixtureResponses.add(fixtureService.convertFixtureToDto(fixture));
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse("success", fixtureResponses));
     }
 }
