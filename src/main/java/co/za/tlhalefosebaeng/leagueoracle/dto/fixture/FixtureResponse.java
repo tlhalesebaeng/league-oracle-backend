2100be0f53b9b5fd84a01e3241e58b3c24139fbb
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-29 22:38:41 +0200

    Define league response dto fields

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/fixture/FixtureResponse.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/fixture/FixtureResponse.java
@@ -1,4 +1,23 @@
 package co.za.tlhalefosebaeng.leagueoracle.dto.fixture;
 
+import co.za.tlhalefosebaeng.leagueoracle.dto.team.TeamResponse;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FixtureResponse {
+    private Long id;
+    private Date date;
+    private String venue;
+    private String field;
+    private String time;
+    private String formattedDate;
+    private String name; // The name of the league this fixture belongs to
+    private TeamResponse homeTeam;
+    private TeamResponse awayTeam;
 }
