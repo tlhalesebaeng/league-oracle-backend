2b8769f10cd18f4132dc6e326e321b6d1aa483bb
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-29 14:25:55 +0200

    Expose an endpoint to generate league fixtures

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/controller/FixtureController.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/controller/FixtureController.java
@@ -1,11 +1,27 @@
 package co.za.tlhalefosebaeng.leagueoracle.controller;
 
+import co.za.tlhalefosebaeng.leagueoracle.model.Fixture;
+import co.za.tlhalefosebaeng.leagueoracle.response.ApiResponse;
+import co.za.tlhalefosebaeng.leagueoracle.service.fixture.FixtureServiceInterface;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("${api.endpoint.prefix}/fixtures")
 public class FixtureController {
+    private final FixtureServiceInterface fixtureService;
+
+    @PostMapping("")
+    public ResponseEntity<ApiResponse> createLeagueFixtures(@RequestParam Long leagueId) {
+        List<Fixture> fixtures = fixtureService.generateLeagueFixtures(leagueId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse("success", fixtures));
+    }
 }
