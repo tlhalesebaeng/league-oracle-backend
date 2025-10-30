eeef379feb2288f832d20f8cd8da9ea9c4a51516
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-30 16:35:28 +0200

    Add data method and constructors to the update request dto

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/fixture/UpdateFixtureRequest.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/fixture/UpdateFixtureRequest.java
@@ -1,7 +1,14 @@
 package co.za.tlhalefosebaeng.leagueoracle.dto.fixture;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UpdateFixtureRequest {
     private Date date;
     private String time;
