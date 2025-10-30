ff51c90110f0acb85294a7a8cc40331859075202
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-30 16:33:59 +0200

    Define update fixture request dto fields

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/fixture/UpdateFixtureRequest.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/fixture/UpdateFixtureRequest.java
@@ -1,4 +1,10 @@
 package co.za.tlhalefosebaeng.leagueoracle.dto.fixture;
 
+import java.util.Date;
+
 public class UpdateFixtureRequest {
+    private Date date;
+    private String time;
+    private String venue;
+    private String field;
 }
