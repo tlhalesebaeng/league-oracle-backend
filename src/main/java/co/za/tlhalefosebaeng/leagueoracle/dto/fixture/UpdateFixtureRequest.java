a3b2c207365f3a1a5c67ddcb4983bd58d7ab09de
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-30 17:24:52 +0200

    Accept the date field as a string

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/fixture/UpdateFixtureRequest.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/fixture/UpdateFixtureRequest.java
@@ -4,13 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateFixtureRequest {
-    private Date date;
+    private String date;
     private String time;
     private String venue;
     private String field;
