d54d91505a8bba6d48b0e988d8a91347271e0d45
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-30 17:30:43 +0200

    Change the date field to a string

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/fixture/FixtureResponse.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/fixture/FixtureResponse.java
@@ -5,18 +5,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class FixtureResponse {
     private Long id;
-    private Date date;
+    private String date;
     private String venue;
     private String field;
     private String time;
-    private String formattedDate;
     private String name; // The name of the league this fixture belongs to
     private TeamResponse homeTeam;
     private TeamResponse awayTeam;
