003bf3386cdb6084135a3e0c081684b4a18dd174
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-31 08:25:23 +0200

    Define a method to convert a result to a result response dto

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/result/ResultServiceInterface.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/result/ResultServiceInterface.java
@@ -1,8 +1,10 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.result;
 
 import co.za.tlhalefosebaeng.leagueoracle.dto.result.AddResultRequest;
+import co.za.tlhalefosebaeng.leagueoracle.dto.result.ResultResponse;
 import co.za.tlhalefosebaeng.leagueoracle.model.Result;
 
 public interface ResultServiceInterface {
+    ResultResponse convertResultToDto(Result result);
     Result addResult(Long fixtureId, AddResultRequest resultRequest);
 }
