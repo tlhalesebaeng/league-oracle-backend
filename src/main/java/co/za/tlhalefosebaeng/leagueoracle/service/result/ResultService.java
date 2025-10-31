f76ff6216c19e1e25ba15a92da59c09f874fd9a6
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-31 07:36:35 +0200

    Implement the result service interface

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/result/ResultService.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/result/ResultService.java
@@ -1,4 +1,12 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.result;
 
-public class ResultService {
+import co.za.tlhalefosebaeng.leagueoracle.model.Result;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ResultService implements ResultServiceInterface {
+    @Override
+    public Result addResult() {
+        return null;
+    }
 }
