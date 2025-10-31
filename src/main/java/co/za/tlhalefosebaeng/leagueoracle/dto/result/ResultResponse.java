0004b2ea70017e3d99915378396b02693ad81f9d
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-31 08:24:33 +0200

    Define fields and add data methods

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/result/ResultResponse.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/result/ResultResponse.java
@@ -1,4 +1,19 @@
 package co.za.tlhalefosebaeng.leagueoracle.dto.result;
 
+import co.za.tlhalefosebaeng.leagueoracle.dto.team.TeamResponse;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ResultResponse {
+    private Long id;
+    private String date;
+    private String time;
+    private Integer homeTeamScore;
+    private Integer awayTeamScore;
+    private TeamResponse homeTeam;
+    private TeamResponse awayTeam;
 }
