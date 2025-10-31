94447f8628cda8a85566d055ab69a1bac5fb57be
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-31 08:50:12 +0200

    Implement custom date and time setter methods

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/result/ResultResponse.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/result/ResultResponse.java
@@ -16,4 +16,34 @@ public class ResultResponse {
     private Integer awayTeamScore;
     private TeamResponse homeTeam;
     private TeamResponse awayTeam;
+
+    public void setDate(String date) {
+        // If the date is not provided, set the date property to "TBC". This should be
+        // the default, but we cannot persist this on the database
+        if(date == null) {
+            this.date = "TBC";
+            return;
+        }
+
+        // Extract the date from the given date string
+        String[] dateTimeDetails = date.split(",");
+
+        // Set the date property to the extracted date details
+        this.date = dateTimeDetails[0];
+    }
+
+    public void setTime(String date) {
+        // If the date is not provided, set the time property to "TBC". This should be
+        // the default, but we cannot persist this on the database
+        if(date == null) {
+            this.time = "TBC";
+            return;
+        }
+
+        // Extract time date from the given date string
+        String[] dateTimeDetails = date.split(",");
+
+        // Set the time property to the extracted time details
+        this.time = dateTimeDetails[1];
+    }
 }
