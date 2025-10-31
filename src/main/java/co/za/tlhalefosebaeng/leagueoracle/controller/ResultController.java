a1170697bfe66ef945a5fbca381edc373a810ca3
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-31 08:26:28 +0200

    Expose an endpoint to add a result

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/controller/ResultController.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/controller/ResultController.java
@@ -1,11 +1,26 @@
 package co.za.tlhalefosebaeng.leagueoracle.controller;
 
+import co.za.tlhalefosebaeng.leagueoracle.dto.result.AddResultRequest;
+import co.za.tlhalefosebaeng.leagueoracle.model.Result;
+import co.za.tlhalefosebaeng.leagueoracle.response.ApiResponse;
+import co.za.tlhalefosebaeng.leagueoracle.service.result.ResultServiceInterface;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("${api.endpoint.prefix}/results")
 public class ResultController {
+    private final ResultServiceInterface resultService;
+
+    @PostMapping("")
+    public ResponseEntity<ApiResponse> addResult(@RequestParam Long fixtureId, @RequestBody AddResultRequest resultRequest) {
+        // Add results of a fixture to the database using the result service
+        Result result = resultService.addResult(fixtureId, resultRequest);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse("success", resultService.convertResultToDto(result)));
+    }
+
 }
