082a735f983383eaa888f00cf5d50e290d957902
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-31 14:23:20 +0200

    Define a jpa finder method to get all results that belong to a certain league

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/repository/ResultRepository.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/repository/ResultRepository.java
@@ -3,5 +3,8 @@ package co.za.tlhalefosebaeng.leagueoracle.repository;
 import co.za.tlhalefosebaeng.leagueoracle.model.Result;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ResultRepository extends JpaRepository<Result, Long> {
+    List<Result> findAllByLeagueId(Long leagueId);
 }
