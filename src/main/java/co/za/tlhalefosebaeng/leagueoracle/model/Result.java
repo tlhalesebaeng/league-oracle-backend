b4177656386b3a676e797d3d812cb0eb353d32ad
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-31 07:18:51 +0200

    Define result properties

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/model/Result.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/model/Result.java
@@ -1,4 +1,10 @@
 package co.za.tlhalefosebaeng.leagueoracle.model;
 
+import java.time.LocalDateTime;
+
 public class Result {
+    private Long id;
+    private LocalDateTime date;
+    private Integer homeTeamScore;
+    private Integer awayTeamScore;
 }
