c754717d038fa2b66d8fa306a8ea5bf299ccd7c5
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-31 08:13:58 +0200

    Define properties and add data methods

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/result/AddResultRequest.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/result/AddResultRequest.java
@@ -1,4 +1,13 @@
 package co.za.tlhalefosebaeng.leagueoracle.dto.result;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AddResultRequest {
+    private Integer homeTeamScore;
+    private Integer awayTeamScore;
 }
