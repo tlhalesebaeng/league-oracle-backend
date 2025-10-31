1ad4d010b2c112145844d47c326e6759e16805d8
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-31 08:43:24 +0200

    Use the app dates static methods to conver dates

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/model/Fixture.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/model/Fixture.java
@@ -1,5 +1,6 @@
 package co.za.tlhalefosebaeng.leagueoracle.model;
 
+import co.za.tlhalefosebaeng.leagueoracle.utils.AppDates;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -7,9 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 
 @Getter
 @Setter
@@ -40,45 +39,16 @@ public class Fixture {
     @JsonIgnore
     private Team awayTeam;
 
-    // The provided date should be in the format yyyy-mm-dd,hh:mm to simplify data extraction
     public void setDate(String date) {
-        // Avoid null pointer exception. There is no need to set the date to null as it will be null by default
-        if(date == null) return;
-
-        // Extract the date and time details from the given string
-        String[] dateTimeDetails = date.split(",");
-        String[] dateDetails = dateTimeDetails[0].split("-");
-        String[] timeDetails = dateTimeDetails[1].split(":");
-
-        // Set the local date using the extracted date details
-        LocalDate localDate = LocalDate.of(
-                Integer.parseInt(dateDetails[0]),
-                Integer.parseInt(dateDetails[1]),
-                Integer.parseInt(dateDetails[2])
-        );
-
-        // Set the local time using the extracted time details
-        LocalTime localTime = LocalTime.of(
-                Integer.parseInt(timeDetails[0]),
-                Integer.parseInt(timeDetails[1])
-        );
-
-        // Set the local date and time property using the local date and time
-        this.date = LocalDateTime.of(localDate, localTime);
+        this.date = AppDates.convertDate(date);
     }
 
-    // The returned date is in the format yyyy-mm-dd,hh:mm but the fixture response formats the
-    // date and time fields so that it can be easier for clients to read
     public String getDate() {
         // Avoid null pointer exception
         if(this.date == null) return null;
 
-        // Get the local date and time from the date property respectively
-        LocalDate localDate = this.date.toLocalDate();
-        LocalTime localTime = this.date.toLocalTime();
-
         // Convert the local date and time to string and return them in the correct format
-        return localDate.toString() + "," + localTime.toString();
+        return AppDates.retrieveDate(this.date.toLocalDate(), this.date.toLocalTime());
     }
 
 }
