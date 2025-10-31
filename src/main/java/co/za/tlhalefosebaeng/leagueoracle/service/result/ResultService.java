5e74b6e4837dc7cf1e52582902bd0327066ecd19
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-31 08:25:54 +0200

    Implement the convert result to result response dto

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/result/ResultService.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/result/ResultService.java
@@ -1,10 +1,12 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.result;
 
 import co.za.tlhalefosebaeng.leagueoracle.dto.result.AddResultRequest;
+import co.za.tlhalefosebaeng.leagueoracle.dto.result.ResultResponse;
 import co.za.tlhalefosebaeng.leagueoracle.model.Fixture;
 import co.za.tlhalefosebaeng.leagueoracle.model.Result;
 import co.za.tlhalefosebaeng.leagueoracle.repository.ResultRepository;
 import co.za.tlhalefosebaeng.leagueoracle.service.fixture.FixtureServiceInterface;
+import co.za.tlhalefosebaeng.leagueoracle.service.team.TeamServiceInterface;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,20 @@ import org.springframework.stereotype.Service;
 public class ResultService implements ResultServiceInterface {
     private final ResultRepository resultRepo;
     private final FixtureServiceInterface fixtureService;
+    private final TeamServiceInterface teamService;
+
+    @Override
+    public ResultResponse convertResultToDto(Result result) {
+        ResultResponse resultResponse = new ResultResponse();
+
+        resultResponse.setId(result.getId());
+        resultResponse.setHomeTeamScore(result.getHomeTeamScore());
+        resultResponse.setAwayTeamScore(result.getAwayTeamScore());
+        resultResponse.setHomeTeam(teamService.convertTeamToDto(result.getHomeTeam()));
+        resultResponse.setAwayTeam(teamService.convertTeamToDto(result.getAwayTeam()));
+
+        return resultResponse;
+    }
 
     @Override
     public Result addResult(Long fixtureId, AddResultRequest resultRequest) {
