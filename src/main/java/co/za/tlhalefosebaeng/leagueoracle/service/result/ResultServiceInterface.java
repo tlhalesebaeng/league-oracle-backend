b3cda389d7beb9e451619563626a4bd10aa601fb
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-10-31 07:36:08 +0200

    Define a method to add results

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/result/ResultServiceInterface.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/result/ResultServiceInterface.java
@@ -1,4 +1,7 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.result;
 
+import co.za.tlhalefosebaeng.leagueoracle.model.Result;
+
 public interface ResultServiceInterface {
+    Result addResult();
 }
