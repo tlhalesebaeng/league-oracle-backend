c546e3d74dc1ed5073b5a4419e22e1eff917ca07
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-05 13:38:28 +0200

    Disable protection against csrf

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/config/AppConfig.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/config/AppConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,6 +14,9 @@ public class AppConfig {
     // Register a security filter chain bean to the application context. This will allow us to configure the filter chain
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // Disable protection against csrf (cross site request forgery)
+        http.csrf(AbstractHttpConfigurer::disable);
+
         return http.build();
     }
 }
