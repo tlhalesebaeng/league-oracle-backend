c92f245dc4a9c46b505a9df8c68193579066963c
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-05 14:01:48 +0200

    Implement a static method that returns an unmodifiable list of protected routes

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/utils/SecuredUrls.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/utils/SecuredUrls.java
@@ -1,4 +1,10 @@
 package co.za.tlhalefosebaeng.leagueoracle.utils;
 
+import java.util.List;
+
 public class SecuredUrls {
+    public static List<String> get() {
+        // Return an unmodifiable list of routes that should be authorized
+        return List.of("");
+    }
 }
