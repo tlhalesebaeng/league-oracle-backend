8d17dd87f388eb1b00c925e6504811dca6f6a1b8
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-05 14:02:41 +0200

    Rename the secured urls file

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/utils/ProtectedRoutes.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/utils/ProtectedRoutes.java
@@ -2,7 +2,7 @@ package co.za.tlhalefosebaeng.leagueoracle.utils;
 
 import java.util.List;
 
-public class SecuredUrls {
+public class ProtectedRoutes {
     public static List<String> get() {
         // Return an unmodifiable list of routes that should be authorized
         return List.of("");
