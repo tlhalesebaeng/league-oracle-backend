d2d3090911f45571c86f83edcc8f495d96b8bfc0
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-05 13:15:25 +0200

    Add annotations to be able to configure security beans

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/config/AppConfig.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/config/AppConfig.java
@@ -1,4 +1,9 @@
 package co.za.tlhalefosebaeng.leagueoracle.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
 public class AppConfig {
 }
