314872032b500f2f71c5b4c26d922c0692de38a6
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-05 13:31:46 +0200

    Register a security filter chain bean within the application context

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/config/AppConfig.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/config/AppConfig.java
@@ -1,9 +1,18 @@
 package co.za.tlhalefosebaeng.leagueoracle.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class AppConfig {
+
+    // Register a security filter chain bean to the application context. This will allow us to configure the filter chain
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.build();
+    }
 }
