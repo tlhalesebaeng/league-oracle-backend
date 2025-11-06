6c1361f9f64069b7ccb6eb218c9320da013b37e6
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-06 22:30:04 +0200

    User the get user by email method to retrieve the user from the database

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/user/AppUserDetailsService.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/user/AppUserDetailsService.java
@@ -1,14 +1,18 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.user;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class AppUserDetailsService implements UserDetailsService {
+    private final UserServiceInterface userService;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        return new AppUserDetails(userService.getUserByEmail(username));
     }
 }
