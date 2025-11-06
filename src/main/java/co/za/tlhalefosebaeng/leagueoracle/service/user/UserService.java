9368274d6da36cb1289e41a5f704c0ba92f96952
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-06 23:37:25 +0200

    Verify that the password and password confirm are the same

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/user/UserService.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/user/UserService.java
@@ -1,11 +1,11 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.user;
 
 import co.za.tlhalefosebaeng.leagueoracle.dto.auth.SignupRequest;
+import co.za.tlhalefosebaeng.leagueoracle.exceptions.PasswordsNotMatchingException;
 import co.za.tlhalefosebaeng.leagueoracle.exceptions.ResourceNotFoundException;
 import co.za.tlhalefosebaeng.leagueoracle.model.User;
 import co.za.tlhalefosebaeng.leagueoracle.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -21,11 +21,16 @@ public class UserService implements UserServiceInterface{
         Optional<User> user = userRepo.findByEmail(email);
 
         // Return the user or throw the relevant exception if the user is not found i.e. the user object is null
-        return user.orElseThrow(() -> new ResourceNotFoundException(HttpStatus.BAD_REQUEST, "User not found! Please the email and try again"));
+        return user.orElseThrow(() -> new ResourceNotFoundException("User not found! Please the email and try again"));
     }
 
     @Override
     public User addUser(SignupRequest user) {
+        // Check that the password and password confirm fields are the same otherwise throw the relevant exception
+        if(!user.getPassword().equals(user.getPasswordConfirm())){
+            throw new PasswordsNotMatchingException("Passwords do not match! Please re-confirm your password");
+        }
+
         // Instantiate a user instance and set the fields accordingly
         User newUser = new User();
         newUser.setFullName(user.getFullName());
