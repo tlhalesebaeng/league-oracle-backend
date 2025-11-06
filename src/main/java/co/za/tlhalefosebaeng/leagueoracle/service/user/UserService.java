aabdee5b5c59b4037aadfbf52dca0ee93275f81b
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-06 22:29:20 +0200

    Implement a method to get a user by email

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/user/UserService.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/user/UserService.java
@@ -1,10 +1,22 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.user;
 
+import co.za.tlhalefosebaeng.leagueoracle.exceptions.ResourceNotFoundException;
 import co.za.tlhalefosebaeng.leagueoracle.model.User;
+import co.za.tlhalefosebaeng.leagueoracle.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
 public class UserService implements UserServiceInterface{
+    private final UserRepository userRepo;
+
     @Override
     public User getUserByEmail(String email) {
-        return null;
+        Optional<User> user = userRepo.findByEmail(email);
+        return user.orElseThrow(() -> new ResourceNotFoundException(HttpStatus.BAD_REQUEST, "User not found! Please the email and try again"));
     }
 }
