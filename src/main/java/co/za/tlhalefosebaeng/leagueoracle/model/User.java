79c4b7bbe9c73972f1bce06def1b6b9e733ea4e1
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-06 21:44:14 +0200

    Define user properties

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/model/User.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/model/User.java
@@ -1,4 +1,8 @@
 package co.za.tlhalefosebaeng.leagueoracle.model;
 
 public class User {
+    private String fullName;
+    private String lastName;
+    private String email;
+    private String password;
 }
