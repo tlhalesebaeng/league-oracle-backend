36303b5a2a073125defedad268eac34f8f6b32aa
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-06 21:45:15 +0200

    Add necessary annotations

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/model/User.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/model/User.java
@@ -1,5 +1,16 @@
 package co.za.tlhalefosebaeng.leagueoracle.model;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class User {
     private String fullName;
     private String lastName;
