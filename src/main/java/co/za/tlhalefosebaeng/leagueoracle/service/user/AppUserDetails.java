a611e881713c6eece9143738334c8e273f151c50
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-06 22:05:02 +0200

    Add a User fields and an all args constructor

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/user/AppUserDetails.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/user/AppUserDetails.java
@@ -1,12 +1,17 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.user;
 
+import co.za.tlhalefosebaeng.leagueoracle.model.User;
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
 
+@AllArgsConstructor
 public class AppUserDetails implements UserDetails {
+    private User user;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of();
