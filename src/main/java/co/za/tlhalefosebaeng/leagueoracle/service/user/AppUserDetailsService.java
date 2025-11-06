ed444ab1907d325e0d8d88732dd1d7c999a052b5
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-06 23:49:29 +0200

    Query the database using the user repo directly to avoid a dependency cycle

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/user/AppUserDetailsService.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/user/AppUserDetailsService.java
@@ -1,5 +1,8 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.user;
 
+import co.za.tlhalefosebaeng.leagueoracle.exceptions.ResourceNotFoundException;
+import co.za.tlhalefosebaeng.leagueoracle.model.User;
+import co.za.tlhalefosebaeng.leagueoracle.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,10 +12,16 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class AppUserDetailsService implements UserDetailsService {
-    private final UserServiceInterface userService;
+    private final UserRepository userRepo;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return new AppUserDetails(userService.getUserByEmail(username));
+        // Get the user from the database using the user repo. We use the user repo directly here to avoid a dependency cycle
+        User user = userRepo
+                .findByEmail(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found! Please your email and try again"));
+
+        // Create an instance of app user details, assign the user field using the constructor and return it
+        return new AppUserDetails(user);
     }
 }
