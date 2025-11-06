c6145906f624f54e6ceae706a8d0d85cb99606eb
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-06 22:58:17 +0200

    Define a method to add a user to the database

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/user/UserServiceInterface.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/user/UserServiceInterface.java
@@ -1,7 +1,9 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.user;
 
+import co.za.tlhalefosebaeng.leagueoracle.dto.auth.SignupRequest;
 import co.za.tlhalefosebaeng.leagueoracle.model.User;
 
 public interface UserServiceInterface {
     User getUserByEmail(String email);
+    User addUser(SignupRequest user);
 }
