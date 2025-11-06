2fe8953e4944caacd5182fc162105b03e4ae4d35
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-06 21:50:59 +0200

    Define an id field and make it auto generated

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/model/User.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/model/User.java
@@ -1,6 +1,9 @@
 package co.za.tlhalefosebaeng.leagueoracle.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +15,10 @@ import lombok.Setter;
 @NoArgsConstructor
 @Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String fullName;
     private String lastName;
     private String email;
