f86b96f1f5789d9864b8e3cf50d6eb218923abef
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-06 20:22:22 +0200

    Implement the user details interface

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/user/AppUserDetails.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/user/AppUserDetails.java
@@ -1,4 +1,24 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.user;
 
-public class AppUserDetails {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class AppUserDetails implements UserDetails {
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
 }
