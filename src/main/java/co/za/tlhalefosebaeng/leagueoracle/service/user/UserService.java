3e254796160e2dbea6cabc5e25949bf130d3df18
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-06 22:13:08 +0200

    Override the get user by email method

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/user/UserService.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/user/UserService.java
@@ -1,4 +1,10 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.user;
 
+import co.za.tlhalefosebaeng.leagueoracle.model.User;
+
 public class UserService implements UserServiceInterface{
+    @Override
+    public User getUserByEmail(String email) {
+        return null;
+    }
 }
