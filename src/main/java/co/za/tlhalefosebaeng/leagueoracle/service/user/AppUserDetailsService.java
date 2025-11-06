9e56d8b950e854a51d47f734d3678644fed49b42
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-06 19:52:39 +0200

    Initial implementation of the user details service

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/user/AppUserDetailsService.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/user/AppUserDetailsService.java
@@ -1,4 +1,12 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.user;
 
-public class AppUserDetailsService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AppUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
