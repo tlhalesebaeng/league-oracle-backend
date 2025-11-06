e84c019e8f3585d99f103ae5cfd0fabd600c3236
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-06 23:35:41 +0200

    Remove the code field and its getters and setters

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/exceptions/PasswordsNotMatchingException.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/exceptions/PasswordsNotMatchingException.java
@@ -1,15 +1,7 @@
 package co.za.tlhalefosebaeng.leagueoracle.exceptions;
 
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.http.HttpStatus;
-
-@Getter
-@Setter
 public class PasswordsNotMatchingException extends RuntimeException{
-    private HttpStatus code;
     public PasswordsNotMatchingException(String message) {
         super(message);
-        this.code = HttpStatus.BAD_REQUEST;
     }
 }
--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/exceptions/ResourceNotFoundException.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/exceptions/ResourceNotFoundException.java
@@ -1,15 +1,7 @@
 package co.za.tlhalefosebaeng.leagueoracle.exceptions;
 
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.http.HttpStatus;
-
-@Getter
-@Setter
 public class ResourceNotFoundException extends RuntimeException {
-    private HttpStatus code;
     public ResourceNotFoundException(String message) {
         super(message);
-        this.code = HttpStatus.BAD_REQUEST;
     }
 }
