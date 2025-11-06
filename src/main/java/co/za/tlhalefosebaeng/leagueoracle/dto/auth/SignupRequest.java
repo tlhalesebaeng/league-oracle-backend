605aaf4278d3fd429c6f360f357b8835f0acce10
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-06 22:38:48 +0200

    Define signup request dto fields

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/auth/SignupRequest.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/auth/SignupRequest.java
@@ -1,4 +1,9 @@
 package co.za.tlhalefosebaeng.leagueoracle.dto.auth;
 
 public class SignupRequest {
+    private String fullName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String passwordConfirm;
 }
