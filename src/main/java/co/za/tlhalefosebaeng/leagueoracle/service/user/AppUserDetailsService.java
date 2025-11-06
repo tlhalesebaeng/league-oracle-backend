c91d0a8d47f30a29d824069b57c7be75a950efc7
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-06 20:05:19 +0200

    Make the user details service a bean

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/user/AppUserDetailsService.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/user/AppUserDetailsService.java
@@ -3,7 +3,9 @@ package co.za.tlhalefosebaeng.leagueoracle.service.user;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AppUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
