4f4199fdf0c8f8137ff76b715c62e1fc32a6ef48
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-06 22:58:45 +0200

    Implement the add user method

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/user/UserService.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/user/UserService.java
@@ -1,5 +1,6 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.user;
 
+import co.za.tlhalefosebaeng.leagueoracle.dto.auth.SignupRequest;
 import co.za.tlhalefosebaeng.leagueoracle.exceptions.ResourceNotFoundException;
 import co.za.tlhalefosebaeng.leagueoracle.model.User;
 import co.za.tlhalefosebaeng.leagueoracle.repository.UserRepository;
@@ -18,5 +19,18 @@ public class UserService implements UserServiceInterface{
     public User getUserByEmail(String email) {
         Optional<User> user = userRepo.findByEmail(email);
         return user.orElseThrow(() -> new ResourceNotFoundException(HttpStatus.BAD_REQUEST, "User not found! Please the email and try again"));
+    }
+
+    @Override
+    public User addUser(SignupRequest user) {
+        // Instantiate a user instance and set the fields accordingly
+        User newUser = new User();
+        newUser.setFullName(user.getFullName());
+        newUser.setLastName(user.getLastName());
+        newUser.setEmail(user.getEmail());
+        newUser.setPassword(user.getPassword());
+
+        // Save the user and return the saved user
+        return userRepo.save(newUser);
     }
 }
