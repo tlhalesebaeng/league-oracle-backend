ccaff082aa2889d0cd636e714552e3fb6c2c496a
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-06 22:34:54 +0200

    Add necessary annotations

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/controller/UserController.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/controller/UserController.java
@@ -1,4 +1,12 @@
 package co.za.tlhalefosebaeng.leagueoracle.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.endpoint.prefix}/auth")
 public class UserController {
 }
