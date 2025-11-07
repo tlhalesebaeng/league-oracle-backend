1c2f39fdabc1537202c3813e15169dc71f5bcf2f
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-07 18:12:08 +0200

    Define a method to generate a jwt

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/jwt/JwtServiceInterface.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/jwt/JwtServiceInterface.java
@@ -1,4 +1,7 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.jwt;
 
+import co.za.tlhalefosebaeng.leagueoracle.model.User;
+
 public interface JwtServiceInterface {
+    String generateToken(User user);
 }
