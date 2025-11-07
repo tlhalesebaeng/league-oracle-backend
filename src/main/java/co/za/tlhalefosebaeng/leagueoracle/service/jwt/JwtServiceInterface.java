6f0659df2d768ec108b8ca4aa45817fa969ce723
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-07 23:04:33 +0200

    Parse spring security's user details to the validate token method

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/jwt/JwtServiceInterface.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/jwt/JwtServiceInterface.java
@@ -1,11 +1,11 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.jwt;
 
 import co.za.tlhalefosebaeng.leagueoracle.model.User;
-import co.za.tlhalefosebaeng.leagueoracle.service.user.AppUserDetails;
 import io.jsonwebtoken.Claims;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface JwtServiceInterface {
     String generateToken(User user);
-    boolean validateToken(String token, AppUserDetails userDetails);
+    boolean validateToken(String token, UserDetails userDetails);
     Claims getAllClaims(String token);
 }
