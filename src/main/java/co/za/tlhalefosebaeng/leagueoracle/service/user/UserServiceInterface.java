0a1c07b261563395b17b2f17bcc2495176daef8f
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-07 20:05:11 +0200

    Define a method to login the user

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/user/UserServiceInterface.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/user/UserServiceInterface.java
@@ -1,5 +1,6 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.user;
 
+import co.za.tlhalefosebaeng.leagueoracle.dto.auth.LoginRequest;
 import co.za.tlhalefosebaeng.leagueoracle.dto.auth.SignupRequest;
 import co.za.tlhalefosebaeng.leagueoracle.dto.auth.UserResponse;
 import co.za.tlhalefosebaeng.leagueoracle.model.User;
@@ -8,4 +9,5 @@ public interface UserServiceInterface {
     User getUserByEmail(String email);
     User addUser(SignupRequest user);
     UserResponse convertUserToDto(User user);
+    User login(LoginRequest details);
 }
