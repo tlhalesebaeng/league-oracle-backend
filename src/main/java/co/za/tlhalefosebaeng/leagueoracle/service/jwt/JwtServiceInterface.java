a41fa3548115d66a1e2237c39b1c067d92552e38
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-07 23:03:31 +0200

    Define a method to extract all claims from a token

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/jwt/JwtServiceInterface.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/jwt/JwtServiceInterface.java
@@ -2,8 +2,10 @@ package co.za.tlhalefosebaeng.leagueoracle.service.jwt;
 
 import co.za.tlhalefosebaeng.leagueoracle.model.User;
 import co.za.tlhalefosebaeng.leagueoracle.service.user.AppUserDetails;
+import io.jsonwebtoken.Claims;
 
 public interface JwtServiceInterface {
     String generateToken(User user);
     boolean validateToken(String token, AppUserDetails userDetails);
+    Claims getAllClaims(String token);
 }
