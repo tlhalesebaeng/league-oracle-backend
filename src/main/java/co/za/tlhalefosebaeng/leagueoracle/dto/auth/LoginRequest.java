86a0b3af6aa072f43e90507c2d4dd3e2775efb8c
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-07 19:41:01 +0200

    Add data methods and necessary constructors

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/auth/LoginRequest.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/auth/LoginRequest.java
@@ -1,5 +1,12 @@
 package co.za.tlhalefosebaeng.leagueoracle.dto.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
     private String email;
     private String password;
