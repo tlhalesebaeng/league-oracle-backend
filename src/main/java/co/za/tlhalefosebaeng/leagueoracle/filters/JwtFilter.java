f07df68a399429f82c574153a24a7b36388fcdc0
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-07 23:06:22 +0200

    Implement the do filter internal method

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/filters/JwtFilter.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/filters/JwtFilter.java
@@ -1,16 +1,50 @@
 package co.za.tlhalefosebaeng.leagueoracle.filters;
 
+import co.za.tlhalefosebaeng.leagueoracle.service.jwt.JwtServiceInterface;
+import co.za.tlhalefosebaeng.leagueoracle.service.user.AppUserDetailsService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@RequiredArgsConstructor
+@Component
 public class JwtFilter extends OncePerRequestFilter {
+    private final JwtServiceInterface jwtService;
+    private final AppUserDetailsService userDetailsService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        
+        String token = null;
+        if(request.getCookies() != null) {
+            for(Cookie cookie : request.getCookies()) {
+                if(cookie.getName().equals("access_jwt")) token = cookie.getName();
+            }
+        }
+
+        String username = null;
+        if(token != null) username = jwtService.getAllClaims(token).getSubject();
+
+        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+            if(jwtService.validateToken(token, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
     }
 }
