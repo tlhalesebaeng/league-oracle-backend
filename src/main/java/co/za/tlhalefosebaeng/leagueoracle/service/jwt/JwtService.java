e663cc417ee0994d5848ae637046d5f827dcd147
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-07 18:08:43 +0200

    Initial commit

---
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/jwt/JwtService.java
@@ -0,0 +1,4 @@
+package co.za.tlhalefosebaeng.leagueoracle.service.jwt;
+
+public class JwtService {
+}
---
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/jwt/JwtServiceInterface.java
@@ -0,0 +1,4 @@
+package co.za.tlhalefosebaeng.leagueoracle.service.jwt;
+
+public interface JwtServiceInterface {
+}
