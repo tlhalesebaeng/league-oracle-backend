24545acf98fade12ce8f26ec30571eabe94ea219
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-07 21:35:51 +0200

    Define a method to validate the given token

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/jwt/JwtServiceInterface.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/jwt/JwtServiceInterface.java
@@ -1,7 +1,9 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.jwt;
 
 import co.za.tlhalefosebaeng.leagueoracle.model.User;
+import co.za.tlhalefosebaeng.leagueoracle.service.user.AppUserDetails;
 
 public interface JwtServiceInterface {
     String generateToken(User user);
+    boolean validateToken(String token, AppUserDetails userDetails);
 }
