14c6ffd0d9d19df408aed3b7427dfffb98e3f8af
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-07 19:09:59 +0200

    Define a method to convert a user object to a user response dto

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/user/UserServiceInterface.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/user/UserServiceInterface.java
@@ -1,9 +1,11 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.user;
 
 import co.za.tlhalefosebaeng.leagueoracle.dto.auth.SignupRequest;
+import co.za.tlhalefosebaeng.leagueoracle.dto.auth.UserResponse;
 import co.za.tlhalefosebaeng.leagueoracle.model.User;
 
 public interface UserServiceInterface {
     User getUserByEmail(String email);
     User addUser(SignupRequest user);
+    UserResponse convertUserToDto(User user);
 }
