b10eb5386db9df76e03623433997ae2e05dd1019
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-07 22:45:30 +0200

    Implement the authentication entry point

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/config/DelegatedAuthenticationEntryPoint.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/config/DelegatedAuthenticationEntryPoint.java
@@ -1,4 +1,16 @@
 package co.za.tlhalefosebaeng.leagueoracle.config;
 
-public class DelegatedAuthenticationEntryPoint {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class DelegatedAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+
+    }
 }
