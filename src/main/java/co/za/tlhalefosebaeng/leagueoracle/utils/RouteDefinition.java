68fdb66a8f7b4f1a6ebdb912f0ee2ca2be3059cc
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-08 12:35:11 +0200

    Make the http method to be of type http status

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/utils/RouteDefinition.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/utils/RouteDefinition.java
@@ -1,9 +1,10 @@
 package co.za.tlhalefosebaeng.leagueoracle.utils;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 public class RouteDefinition {
     private String uri;
-    private String method;
+    private HttpStatus method;
 }
