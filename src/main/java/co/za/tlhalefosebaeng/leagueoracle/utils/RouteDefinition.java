b55b0728df17dbdf0d4478ed664e81910f127569
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-08 12:35:47 +0200

    Add getters only

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/utils/RouteDefinition.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/utils/RouteDefinition.java
@@ -1,8 +1,10 @@
 package co.za.tlhalefosebaeng.leagueoracle.utils;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
 @AllArgsConstructor
 public class RouteDefinition {
     private String uri;
