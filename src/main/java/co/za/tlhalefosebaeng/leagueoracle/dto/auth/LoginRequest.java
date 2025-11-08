43b400d3df60c9ce643d73a9dae13a24a65ef3a1
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-08 08:31:25 +0200

    Validate fields

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/auth/LoginRequest.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/auth/LoginRequest.java
@@ -1,5 +1,6 @@
 package co.za.tlhalefosebaeng.leagueoracle.dto.auth;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginRequest {
+    @NotBlank(message = "Missing field! All fields are required")
     private String email;
+
+    @NotBlank(message = "Missing field! All fields are required")
     private String password;
 }
--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/auth/SignupRequest.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/auth/SignupRequest.java
@@ -1,5 +1,6 @@
 package co.za.tlhalefosebaeng.leagueoracle.dto.auth;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,9 +9,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SignupRequest {
+    @NotBlank(message = "First name required! Please provide your first name")
     private String fullName;
+
+    @NotBlank(message = "Last name required! Please provide your last name")
     private String lastName;
+
+    @NotBlank(message = "Email required! Please provide a valid email address")
     private String email;
+
+    @NotBlank(message = "Password required! Please provide a password")
     private String password;
+
+    @NotBlank(message = "Password confirmation required! Please confirm your password")
     private String passwordConfirm;
 }
