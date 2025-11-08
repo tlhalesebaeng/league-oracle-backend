e78724b3306ebc18019e3348af65228d97ad3e72
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-08 12:29:51 +0200

    Return an immutable list of protected routes

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/utils/ProtectedRoutes.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/utils/ProtectedRoutes.java
@@ -3,8 +3,8 @@ package co.za.tlhalefosebaeng.leagueoracle.utils;
 import java.util.List;
 
 public class ProtectedRoutes {
-    public static List<String> get() {
+    public static List<RouteDefinition> get() {
         // Return an unmodifiable list of routes that should be authorized
-        return List.of("");
+        return List.of();
     }
 }
