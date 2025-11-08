ac615f2837d15c00fffb4939a05dbb64bd24d556
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-08 12:26:38 +0200

    Add a constructor for all arguments

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/utils/RouteDefinition.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/utils/RouteDefinition.java
@@ -1,5 +1,8 @@
 package co.za.tlhalefosebaeng.leagueoracle.utils;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class RouteDefinition {
     private String uri;
     private String method;
