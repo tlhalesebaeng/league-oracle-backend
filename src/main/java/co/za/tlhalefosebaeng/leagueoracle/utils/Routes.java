9e3e29e64d3db97026b1e01365d4e14b530477c2
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-08 13:22:06 +0200

    Implement a method to check if a route is part of the protected routes

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/utils/Routes.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/utils/Routes.java
@@ -7,4 +7,17 @@ public class Routes {
         // Return an unmodifiable list of routes that should be authorized
         return List.of();
     }
+
+    // Helper method to check if a route is part of the protected routes
+    public static boolean isProtected(String method, String URI) {
+        for(RouteDefinition route : getProtected()) {
+            // Check if the route is part of the protected routes. The request should have similar URI and http method as the protected route
+            if(route.getMethod().matches(method) && route.getURI().equals(URI)){
+                return true; // The route is found so it is protected
+            }
+        }
+
+        // All checks are successful, the request is not protected
+        return false;
+    }
 }
