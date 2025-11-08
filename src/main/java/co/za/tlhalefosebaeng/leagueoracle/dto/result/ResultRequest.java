4b16ecbd519ae44696687d39c5fc15ee483089f7
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-08 09:26:43 +0200

    Validate request fields

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/result/ResultRequest.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/result/ResultRequest.java
@@ -1,5 +1,6 @@
 package co.za.tlhalefosebaeng.leagueoracle.dto.result;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ResultRequest {
+    @NotNull(message = "Home team score required! Please provide home team score")
     private Integer homeTeamScore;
+
+    @NotNull(message = "Away team score required! Please provide away team score")
     private Integer awayTeamScore;
 }
