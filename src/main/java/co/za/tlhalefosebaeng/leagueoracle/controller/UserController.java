62d9b035342159a46c4184d4746f60dbc97a79a3
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-09 14:46:50 +0200

    Use the newly created cookie service to create a configured cookie

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/controller/UserController.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/controller/UserController.java
@@ -6,6 +6,7 @@ import co.za.tlhalefosebaeng.leagueoracle.model.User;
 import co.za.tlhalefosebaeng.leagueoracle.response.ApiResponse;
 import co.za.tlhalefosebaeng.leagueoracle.service.jwt.JwtServiceInterface;
 import co.za.tlhalefosebaeng.leagueoracle.service.user.UserServiceInterface;
+import co.za.tlhalefosebaeng.leagueoracle.service.cookie.CookieService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -23,15 +24,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserServiceInterface userService;
     private final JwtServiceInterface jwtService;
+    private final CookieService cookieService;
 
     @PostMapping("/signup")
     public ResponseEntity<ApiResponse> signup(@Valid @RequestBody SignupRequest user, HttpServletResponse response){
         // Sign the user up using the user service
         User newUser = userService.addUser(user);
 
-        // Generate the token and set it as a cookie in the response
-        String jwt = jwtService.generateToken(newUser);
-        response.addCookie(new Cookie("access_jwt", jwt));
+        String jwt = jwtService.generateToken(newUser); // Generate the token using the jwt service
+        response.addCookie(cookieService.create("access_jwt", jwt)); // Create a cookie using the cookie service and add it to the response
 
         // Convert the user object to a user response dto and return it as part of the api response
         return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse("success", userService.convertUserToDto(newUser)));
@@ -44,7 +45,7 @@ public class UserController {
 
         // Generate the token and set it as a cookie in the response
         String jwt = jwtService.generateToken(user);
-        response.addCookie(new Cookie("access_jwt", jwt));
+        response.addCookie(cookieService.create("access_jwt", jwt));
 
         // Convert the user object to a user response dto and return it as part of the api response
         return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse("success", userService.convertUserToDto(user)));
