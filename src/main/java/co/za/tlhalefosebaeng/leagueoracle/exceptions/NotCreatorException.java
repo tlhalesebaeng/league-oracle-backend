6fe4e7226616aa08bfa9095be24ba8c3748b8927
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-09 09:26:11 +0200

    Delete not creator exception

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/exceptions/NotCreatorException.java
+++
@@ -1,7 +0,0 @@
-package co.za.tlhalefosebaeng.leagueoracle.exceptions;
-
-public class NotCreatorException extends RuntimeException {
-    public NotCreatorException(String message) {
-        super(message);
-    }
-}
