7e7cd6f1e1462f7ac253d51d7a4312e12d76ff91
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-09 09:14:21 +0200

    Add gettes

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/exceptions/AppException.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/exceptions/AppException.java
@@ -1,7 +1,9 @@
 package co.za.tlhalefosebaeng.leagueoracle.exceptions;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
 public class AppException extends RuntimeException {
     private HttpStatus status;
     private String message;
