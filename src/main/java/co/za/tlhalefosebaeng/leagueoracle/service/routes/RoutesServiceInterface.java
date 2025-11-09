0067a02ed09a3455bde31c7f7bef15c5c21c89bf
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-09 14:53:26 +0200

    Define methods to get protected routes and to check if the route is protected

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/routes/RoutesServiceInterface.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/routes/RoutesServiceInterface.java
@@ -1,4 +1,10 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.routes;
 
+import co.za.tlhalefosebaeng.leagueoracle.utils.RouteDefinition;
+
+import java.util.List;
+
 public interface RoutesServiceInterface {
+    List<RouteDefinition> getProtected();
+    boolean isProtected(String method, String URI);
 }
