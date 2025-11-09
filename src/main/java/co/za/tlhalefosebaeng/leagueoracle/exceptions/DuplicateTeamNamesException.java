18da813f71d02ac9c1ed0917de5e8016b6db0740
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-09 09:27:27 +0200

    Delete duplicate team names exception

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/exceptions/DuplicateTeamNamesException.java
+++
@@ -1,7 +0,0 @@
-package co.za.tlhalefosebaeng.leagueoracle.exceptions;
-
-public class DuplicateTeamNamesException extends RuntimeException {
-    public DuplicateTeamNamesException(String message) {
-        super(message);
-    }
-}
