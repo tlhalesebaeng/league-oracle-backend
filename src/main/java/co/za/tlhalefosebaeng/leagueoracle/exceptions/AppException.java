ff52ef6512411a51a0b9d7022721796299877503
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-09 09:20:29 +0200

    Add a constructor that accepts a status and an error message and assign the status and calls the parent class with the message

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/exceptions/AppException.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/exceptions/AppException.java
@@ -6,5 +6,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 public class AppException extends RuntimeException {
     private HttpStatus status;
-    private String message;
+
+    public AppException(HttpStatus status, String message) {
+        super(message);
+        this.status = status;
+    }
 }
