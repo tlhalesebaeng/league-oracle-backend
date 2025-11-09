b3ab1a3f01832b1cb4a172641c178e990c867b39
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-09 09:29:03 +0200

    Delete incorrect credentials exception

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/exceptions/IncorrectCredentialsException.java
+++
@@ -1,7 +0,0 @@
-package co.za.tlhalefosebaeng.leagueoracle.exceptions;
-
-public class IncorrectCredentialsException extends RuntimeException {
-    public IncorrectCredentialsException(String message) {
-        super(message);
-    }
-}
