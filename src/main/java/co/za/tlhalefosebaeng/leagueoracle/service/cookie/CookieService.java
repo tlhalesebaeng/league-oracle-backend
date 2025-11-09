e0f3386fafd14de3f876cd81c77f86914664df3b
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-09 14:44:57 +0200

    Implement the create method

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/cookie/CookieService.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/cookie/CookieService.java
@@ -1,4 +1,24 @@
-package co.za.tlhalefosebaeng.leagueoracle.utils;
+package co.za.tlhalefosebaeng.leagueoracle.service.cookie;
 
-public class AppCookie {
+import jakarta.servlet.http.Cookie;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CookieService implements CookieServiceInterface{
+    @Value("${api.environment}")
+    private String environment;
+
+    // Helper method that creates a cookie with all the necessary configuration
+    @Override
+    public Cookie create(String name, String value) {
+        Cookie cookie = new Cookie(name, value); // Create a new instance of a cookie
+        cookie.isHttpOnly(); // Make the cookie http only
+
+        // When the server environment is not development make the cookie secure
+        if(!environment.equals("development")) cookie.setSecure(true);
+
+        // When all configuration are done return the cookie
+        return cookie;
+    }
 }
