1c719e00b2e8d33be0ad822d8654be7768223fb5
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-09 14:44:17 +0200

    Define a method that creates a cookie

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/cookie/CookieServiceInterface.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/cookie/CookieServiceInterface.java
@@ -1,4 +1,7 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.cookie;
 
+import jakarta.servlet.http.Cookie;
+
 public interface CookieServiceInterface {
+    Cookie create(String name, String value);
 }
