0d0ededfe5d4f4098ea3d4b3b4bf3741fa21e5ae
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-10 14:36:13 +0200

    Add data methods and all args constructor

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/auth/AuthResponse.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/auth/AuthResponse.java
@@ -1,5 +1,10 @@
 package co.za.tlhalefosebaeng.leagueoracle.dto.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class AuthResponse {
     private UserResponse user;
 }
