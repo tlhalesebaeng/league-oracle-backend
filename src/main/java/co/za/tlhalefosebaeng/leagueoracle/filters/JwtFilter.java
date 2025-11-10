94298f717abdf451ef189a696aa04c5976244cc5
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-10 09:13:37 +0200

    Catch all the JWT exception and call the filter exception resolver with the exceptions

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/filters/JwtFilter.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/filters/JwtFilter.java
@@ -3,18 +3,25 @@ package co.za.tlhalefosebaeng.leagueoracle.filters;
 import co.za.tlhalefosebaeng.leagueoracle.service.jwt.JwtServiceInterface;
 import co.za.tlhalefosebaeng.leagueoracle.service.user.AppUserDetailsService;
 import co.za.tlhalefosebaeng.leagueoracle.service.routes.RoutesService;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 
@@ -24,40 +31,50 @@ public class JwtFilter extends OncePerRequestFilter {
     private final JwtServiceInterface jwtService;
     private final AppUserDetailsService userDetailsService;
     private final RoutesService routesService;
+    private final @Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        // Do not perform this filter for routes that are not protected
-        if(!routesService.isProtected(request.getMethod(), request.getRequestURI())) {
-            filterChain.doFilter(request, response); // Perform the next filter in the filter chain
-            return;
-        }
-
-        // Get the token from the cookie
-        String token = null;
-        if(request.getCookies() != null) {
-            for(Cookie cookie : request.getCookies()) {
-                if(cookie.getName().equals("access_jwt")) token = cookie.getValue();
+        try {
+            // Do not perform this filter for routes that are not protected
+            if(!routesService.isProtected(request.getMethod(), request.getRequestURI())) {
+                filterChain.doFilter(request, response); // Perform the next filter in the filter chain
+                return;
             }
-        }
 
-        // Get the username from the token
-        String username = null;
-        if(token != null) username = jwtService.getAllClaims(token).getSubject();
-
-        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            // Get the user from the database using the username. This will confirm that the user exists
-            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-
-            // Validate the token and add the user to the security context holder
-            if(jwtService.validateToken(token, userDetails)) {
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authToken);
+            // Get the token from the cookie
+            String token = null;
+            if(request.getCookies() != null) {
+                for(Cookie cookie : request.getCookies()) {
+                    if(cookie.getName().equals("access_jwt")) token = cookie.getValue();
+                }
             }
-        }
 
-        // Perform the next filter in the chain
-        filterChain.doFilter(request, response);
+            // Get the username from the token
+            String username = null;
+            if(token != null) username = jwtService.getAllClaims(token).getSubject();
+
+            if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                // Get the user from the database using the username. This will confirm that the user exists
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+                // Validate the token and add the user to the security context holder
+                if(jwtService.validateToken(token, userDetails)) {
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            }
+
+            // Perform the next filter in the chain
+            filterChain.doFilter(request, response);
+        } catch (ExpiredJwtException | MalformedJwtException |
+                 SignatureException | IllegalArgumentException |
+                 UnsupportedJwtException jwtException)
+        {
+            resolver.resolveException(request, response, null, jwtException);
+        } catch (IOException | ServletException | UsernameNotFoundException  e) {
+            throw new RuntimeException(e);
+        }
     }
 }
