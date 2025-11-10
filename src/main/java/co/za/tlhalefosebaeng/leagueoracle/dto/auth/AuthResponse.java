b445a4206ff88d8ab92fbd937655980867db952d
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-10 15:07:05 +0200

    Add an all args constructor

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/auth/AuthResponse.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/auth/AuthResponse.java
@@ -1,8 +1,10 @@
 package co.za.tlhalefosebaeng.leagueoracle.dto.auth;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class AuthResponse {
     private boolean isAuth;
     private UserResponse user;
