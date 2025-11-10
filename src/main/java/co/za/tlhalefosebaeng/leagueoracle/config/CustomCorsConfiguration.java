2248f61c7f5137912cf9f46cd4af29291c28a017
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-10 14:15:20 +0200

    Add custom cors configuration

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/config/CustomCorsConfiguration.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/config/CustomCorsConfiguration.java
@@ -5,10 +5,21 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+import java.util.List;
+
 @Component
 public class CustomCorsConfiguration implements CorsConfigurationSource {
     @Override
     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-        return null;
+        // Create a new instance of the cors configuration class
+        CorsConfiguration corsConfig = new CorsConfiguration();
+
+        corsConfig.setAllowCredentials(true); // Allow cookies and other credentials to be included
+        corsConfig.setAllowedOrigins(List.of("http://localhost:5173")); // Set the allowed domains
+        corsConfig.setAllowedMethods(List.of("*")); // Allow all http request methods
+        corsConfig.setAllowedHeaders(List.of("*")); // Allow all request headers
+
+        // Return the configuration instance
+        return corsConfig;
     }
 }
