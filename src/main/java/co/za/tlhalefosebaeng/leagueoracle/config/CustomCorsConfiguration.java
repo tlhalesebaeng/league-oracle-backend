6d8e1b729f9834dedf29eafd910550269c080594
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-10 14:10:28 +0200

    Add the annotation that makes the class a bean

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/config/CustomCorsConfiguration.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/config/CustomCorsConfiguration.java
@@ -1,9 +1,11 @@
 package co.za.tlhalefosebaeng.leagueoracle.config;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+@Component
 public class CustomCorsConfiguration implements CorsConfigurationSource {
     @Override
     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
