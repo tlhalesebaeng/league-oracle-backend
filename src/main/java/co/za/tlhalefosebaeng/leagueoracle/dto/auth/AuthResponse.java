f3f0d9eec77547e888e0b5bd2c4b80aec84dd2d5
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-10 15:02:47 +0200

    Remove the all args constructor

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/auth/AuthResponse.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/auth/AuthResponse.java
@@ -1,10 +1,8 @@
 package co.za.tlhalefosebaeng.leagueoracle.dto.auth;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class AuthResponse {
     private UserResponse user;
 }
