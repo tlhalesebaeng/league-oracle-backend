4528bd7dd426940238eab0123e7d5a42097503c7
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-28 00:09:32 +0200

    Add a method to convert league objects to league response dtos

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/mapper/LeagueMapper.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/mapper/LeagueMapper.java
@@ -1,4 +1,23 @@
 package co.za.tlhalefosebaeng.leagueoracle.mapper;
 
+import co.za.tlhalefosebaeng.leagueoracle.dto.league.LeagueResponse;
+import co.za.tlhalefosebaeng.leagueoracle.model.League;
+
+import java.time.LocalDate;
+
 public class LeagueMapper {
+    // Method used to convert league objects to league response dtos
+    public static LeagueResponse toResponse(League league) {
+        LeagueResponse response = new LeagueResponse();
+        response.setId(league.getId());
+        response.setName(league.getName());
+        response.setCreator(league.getCreator().getId());
+        response.setTeams(TeamMapper.toResponseList(league.getTeams()));
+
+        // Convert the date the league was created at to a local date object
+        LocalDate date = league.getCreatedAt().toLocalDate();
+        response.setCreatedAt(date.toString()); // Get the yyyy-mm-dd format of the local date
+
+        return response;
+    }
 }
