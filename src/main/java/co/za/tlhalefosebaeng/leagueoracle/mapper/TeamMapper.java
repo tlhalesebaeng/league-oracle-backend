e4b80907ac490093b6fb91d9c4566ff5dfd880d7
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-27 23:57:02 +0200

    Add a method to convert a team to a team response dto

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/mapper/TeamMapper.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/mapper/TeamMapper.java
@@ -1,4 +1,22 @@
 package co.za.tlhalefosebaeng.leagueoracle.mapper;
 
+import co.za.tlhalefosebaeng.leagueoracle.dto.team.TeamResponse;
+import co.za.tlhalefosebaeng.leagueoracle.model.Team;
+
 public class TeamMapper {
+    // Method to convert team objects to team response dtos
+    public static TeamResponse toResponse(Team team) {
+        TeamResponse response = new TeamResponse();
+        response.setId(team.getId());
+        response.setName(team.getName());
+        response.setWins(team.getWins());
+        response.setDraws(team.getDraws());
+        response.setLoses(team.getLoses());
+        response.setGoalsForward(team.getGoalsForward());
+        response.setGoalsAgainst(team.getGoalsAgainst());
+        response.setPlayedGames();
+        response.setPoints();
+        response.setGoalDifference();
+        return response;
+    }
 }
