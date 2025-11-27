1ab38436691befc42d2409eecca2e2239245eb95
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-28 00:07:55 +0200

    Remove the method to convert league objects to league response dtos

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/league/LeagueServiceInterface.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/league/LeagueServiceInterface.java
@@ -1,14 +1,9 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.league;
 
 import co.za.tlhalefosebaeng.leagueoracle.dto.league.LeagueRequest;
-import co.za.tlhalefosebaeng.leagueoracle.dto.league.LeagueResponse;
-import co.za.tlhalefosebaeng.leagueoracle.dto.team.AddTeamRequest;
-import co.za.tlhalefosebaeng.leagueoracle.dto.team.TeamResponse;
 import co.za.tlhalefosebaeng.leagueoracle.model.League;
-import co.za.tlhalefosebaeng.leagueoracle.model.Team;
 
 import java.util.List;
-import java.util.function.Function;
 
 public interface LeagueServiceInterface {
     League saveLeague(League league);
@@ -17,7 +12,6 @@ public interface LeagueServiceInterface {
     List<League> getAllLeagues(String name);
     League updateLeague(Long leagueId, LeagueRequest requestDto);
     void deleteLeague(Long leagueId);
-    LeagueResponse convertLeagueToDto(League league, Function<Team, TeamResponse> convertTeamToDto);
     boolean isCreator(League league);
     List<League> getMyLeagues();
 }
