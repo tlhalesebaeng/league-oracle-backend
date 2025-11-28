9aa4c94a04c66c11c9659e98ecad2d81732b0c24
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-28 13:57:56 +0200

    Add a method to find all teams by league id

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/repository/TeamRepository.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/repository/TeamRepository.java
@@ -3,5 +3,8 @@ package co.za.tlhalefosebaeng.leagueoracle.repository;
 import co.za.tlhalefosebaeng.leagueoracle.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TeamRepository extends JpaRepository<Team, Long> {
+    List<Team> findAllByLeagueId(Long id);
 }
