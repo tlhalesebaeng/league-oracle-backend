93314fa45a34f38824fca1a1a0457cc80950fe4f
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-28 12:49:29 +0200

    Remove the method to convert user objects to user response dtos

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/user/UserServiceInterface.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/user/UserServiceInterface.java
@@ -8,7 +8,6 @@ import co.za.tlhalefosebaeng.leagueoracle.model.User;
 public interface UserServiceInterface {
     User getUserByEmail(String email);
     User addUser(SignupRequest user);
-    UserResponse convertUserToDto(User user);
     User login(LoginRequest details);
     User checkAuth(String jwt);
 }
