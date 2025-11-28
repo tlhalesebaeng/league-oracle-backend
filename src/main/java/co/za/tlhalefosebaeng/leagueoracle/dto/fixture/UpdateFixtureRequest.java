fa0f0bf31277a6bb2989bc18c163d5aa8e9ddb05
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-28 12:11:23 +0200

    Add a pattern to be matched by the date and time fields

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/fixture/UpdateFixtureRequest.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/fixture/UpdateFixtureRequest.java
@@ -1,5 +1,6 @@
 package co.za.tlhalefosebaeng.leagueoracle.dto.fixture;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +9,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateFixtureRequest {
+    @Pattern(regexp = "^(?:19|20)\\d\\d-(?:0[1-9]|1[0-2])-(?:0[1-9]|[12]\\d|3[01])$", message = "Invalid date provided! Please check your date and check again")
     private String date;
+
+    @Pattern(regexp = "^(?:[01]\\d|2[0-3]):[0-5]\\d$", message = "Invalid date provided! Please check your date and check again")
     private String time;
+
     private String venue;
     private String field;
 }
