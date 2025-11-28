e473e0962937a107afeb0eefdd81328854874a10
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-28 12:44:19 +0200

    Create a method to convert user objects to user response dtos

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/mapper/UserMapper.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/mapper/UserMapper.java
@@ -1,4 +1,17 @@
 package co.za.tlhalefosebaeng.leagueoracle.mapper;
 
+import co.za.tlhalefosebaeng.leagueoracle.dto.auth.UserResponse;
+import co.za.tlhalefosebaeng.leagueoracle.model.User;
+
 public class UserMapper {
+    public static UserResponse toResponse(User user) {
+        UserResponse userResponse = new UserResponse();
+
+        userResponse.setId(user.getId());
+        userResponse.setFirstName(user.getFirstName());
+        userResponse.setLastName(user.getLastName());
+        userResponse.setEmail(user.getEmail());
+
+        return userResponse;
+    }
 }
