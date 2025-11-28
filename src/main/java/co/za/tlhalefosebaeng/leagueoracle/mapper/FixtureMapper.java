bb5f2e366a9ad7ab530872da19ea15288e1485b2
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-28 12:18:35 +0200

    Add a method to convert fixture objects to fixture response dtos

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/mapper/FixtureMapper.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/mapper/FixtureMapper.java
@@ -1,4 +1,21 @@
 package co.za.tlhalefosebaeng.leagueoracle.mapper;
 
+import co.za.tlhalefosebaeng.leagueoracle.dto.fixture.FixtureResponse;
+import co.za.tlhalefosebaeng.leagueoracle.model.Fixture;
+
 public class FixtureMapper {
+    public static FixtureResponse toResponse(Fixture fixture) {
+        FixtureResponse response = new FixtureResponse();
+
+        response.setId(fixture.getId());
+        response.setLeague(fixture.getLeague().getId());
+        response.setDate(fixture.getDate());
+        response.setTime(fixture.getTime());
+        response.setVenue(fixture.getVenue());
+        response.setField(fixture.getField());
+        response.setHomeTeam(TeamMapper.toResponse(fixture.getHomeTeam()));
+        response.setAwayTeam(TeamMapper.toResponse(fixture.getAwayTeam()));
+
+        return response;
+    }
 }
