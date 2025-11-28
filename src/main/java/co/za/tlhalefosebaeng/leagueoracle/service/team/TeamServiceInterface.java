09dfad0b314f5a59691772b8c2ff4f8cad96033c
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-28 13:56:51 +0200

    Add a method to get all the teams that belong to a league

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/team/TeamServiceInterface.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/team/TeamServiceInterface.java
@@ -2,12 +2,14 @@ package co.za.tlhalefosebaeng.leagueoracle.service.team;
 
 import co.za.tlhalefosebaeng.leagueoracle.dto.team.AddTeamRequest;
 import co.za.tlhalefosebaeng.leagueoracle.dto.team.UpdateTeamRequest;
-import co.za.tlhalefosebaeng.leagueoracle.entity.League;
 import co.za.tlhalefosebaeng.leagueoracle.entity.Team;
+
+import java.util.List;
 
 public interface TeamServiceInterface {
     Team addTeam(Long leagueId, AddTeamRequest requestDto);
     Team getTeam(Long teamId);
     Team updateTeam(Long teamId, UpdateTeamRequest requestDto);
+    List<Team> getAllLeagueTeams(Long leagueId);
     void deleteTeam(Long teamId);
 }
