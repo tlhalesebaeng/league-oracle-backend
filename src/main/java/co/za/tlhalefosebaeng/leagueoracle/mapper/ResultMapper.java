083ad3ae1de52675bd2bacd8c5dcc96197f9eede
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-28 12:31:24 +0200

    Create a method to convert result objects to result response dtos

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/mapper/ResultMapper.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/mapper/ResultMapper.java
@@ -1,4 +1,21 @@
 package co.za.tlhalefosebaeng.leagueoracle.mapper;
 
+import co.za.tlhalefosebaeng.leagueoracle.dto.result.ResultResponse;
+import co.za.tlhalefosebaeng.leagueoracle.model.Result;
+
 public class ResultMapper {
+    public static ResultResponse toResponse(Result result) {
+        ResultResponse response = new ResultResponse();
+
+        response.setId(result.getId());
+        response.setName(result.getLeague().getName());
+        response.setDate(result.getDate());
+        response.setTime(result.getTime());
+        response.setHomeTeamScore(result.getHomeTeamScore());
+        response.setAwayTeamScore(result.getAwayTeamScore());
+        response.setHomeTeam(TeamMapper.toResponse(result.getHomeTeam()));
+        response.setAwayTeam(TeamMapper.toResponse(result.getAwayTeam()));
+
+        return response;
+    }
 }
