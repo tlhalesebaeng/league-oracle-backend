c54864011e79e0dd337d3bbd6d5c17b334074570
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-28 13:58:16 +0200

    Expose an endpoint to get all teams that belong to a league

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/controller/TeamController.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/controller/TeamController.java
@@ -12,6 +12,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("${api.endpoint.prefix}/teams")
@@ -22,6 +24,13 @@ public class TeamController {
     public ResponseEntity<TeamResponse> addTeam(@RequestParam Long leagueId, @Valid @RequestBody AddTeamRequest requestDto){
         Team team = teamService.addTeam(leagueId, requestDto);
         TeamResponse responseDto = TeamMapper.toResponse(team);
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<TeamResponse>> getAllLeagueTeams(@RequestParam Long leagueId) {
+        List<Team> teams = teamService.getAllLeagueTeams(leagueId);
+        List<TeamResponse> responseDto = TeamMapper.toResponseList(teams);
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
