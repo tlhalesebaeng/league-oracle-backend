a13e980de6f5f5b35a8f24ea94f2f202fc308bf0
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-28 13:46:09 +0200

    Remove team fields that should be updated through results

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/team/UpdateTeamRequest.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/dto/team/UpdateTeamRequest.java
@@ -1,28 +1,12 @@
 package co.za.tlhalefosebaeng.leagueoracle.dto.team;
 
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class UpdateTeamRequest {
-    @Pattern(regexp = "^(?!\\s*$).+", message = "Invalid name! Please provide a non empty name")
+    @NotBlank(message = "Invalid name! Please provide a valid name")
     private String name;
-
-    @PositiveOrZero(message = "Invalid wins! Please provide a positive value for wins")
-    private int wins;
-
-    @PositiveOrZero(message = "Invalid draws! Please provide a positive value for draws")
-    private int draws;
-
-    @PositiveOrZero(message = "Invalid loses! Please provide a positive value for loses")
-    private int loses;
-
-    @PositiveOrZero(message = "Invalid goals forward! Please provide a positive value for goals forward")
-    private int goalsForward;
-
-    @PositiveOrZero(message = "Invalid goals against! Please provide a positive value for goals against")
-    private int goalsAgainst;
 }
