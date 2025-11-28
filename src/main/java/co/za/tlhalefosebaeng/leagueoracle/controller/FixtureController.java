8e215c9201346e6342a05111d11f882e2f56d3ba
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-28 12:26:12 +0200

    Use the newly created fixture mapper to convert fixture objects to fixture response dtos

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/controller/FixtureController.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/controller/FixtureController.java
@@ -2,6 +2,7 @@ package co.za.tlhalefosebaeng.leagueoracle.controller;
 
 import co.za.tlhalefosebaeng.leagueoracle.dto.fixture.FixtureResponse;
 import co.za.tlhalefosebaeng.leagueoracle.dto.fixture.UpdateFixtureRequest;
+import co.za.tlhalefosebaeng.leagueoracle.mapper.FixtureMapper;
 import co.za.tlhalefosebaeng.leagueoracle.model.Fixture;
 import co.za.tlhalefosebaeng.leagueoracle.service.fixture.FixtureServiceInterface;
 import jakarta.validation.Valid;
@@ -10,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -22,45 +22,36 @@ public class FixtureController {
     @PostMapping("")
     public ResponseEntity<List<FixtureResponse>> createLeagueFixtures(@RequestParam Long leagueId) {
         List<Fixture> fixtures = fixtureService.generateLeagueFixtures(leagueId);
-        List<FixtureResponse> fixtureResponses = new ArrayList<>();
-        for(Fixture fixture : fixtures) {
-            fixtureResponses.add(fixtureService.convertFixtureToDto(fixture));
-        }
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(fixtureResponses);
+        List<FixtureResponse> responseDto = FixtureMapper.toResponseList(fixtures);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
     }
 
     @GetMapping("")
     public ResponseEntity<List<FixtureResponse>> getAllLeagueFixtures(@RequestParam Long leagueId) {
         List<Fixture> fixtures = fixtureService.getAllLeagueFixtures(leagueId);
-        List<FixtureResponse> fixtureResponses = new ArrayList<>();
-        for(Fixture fixture : fixtures) {
-            fixtureResponses.add(fixtureService.convertFixtureToDto(fixture));
-        }
-
-        return ResponseEntity.status(HttpStatus.OK).body(fixtureResponses);
+        List<FixtureResponse> responseDto = FixtureMapper.toResponseList(fixtures);
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
     @GetMapping("/{fixtureId}")
     public ResponseEntity<FixtureResponse> getFixture(@PathVariable Long fixtureId) {
         Fixture fixture = fixtureService.getFixture(fixtureId);
-        return ResponseEntity.status(HttpStatus.OK).body(fixtureService.convertFixtureToDto(fixture));
+        FixtureResponse responseDto = FixtureMapper.toResponse(fixture);
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
     @GetMapping("/upcoming")
     public ResponseEntity<List<FixtureResponse>> getUpcomingFixtures(@RequestParam Integer month) {
         List<Fixture> fixtures = fixtureService.getUpcomingFixtures(month);
-        List<FixtureResponse> fixtureResponses = new ArrayList<>();
-        for(Fixture fixture : fixtures) {
-            fixtureResponses.add(fixtureService.convertFixtureToDto(fixture));
-        }
-        return ResponseEntity.status(HttpStatus.OK).body(fixtureResponses);
+        List<FixtureResponse> responseDto = FixtureMapper.toResponseList(fixtures);
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
     @PatchMapping("/{fixtureId}")
     public ResponseEntity<FixtureResponse> updateFixture(@PathVariable Long fixtureId, @Valid @RequestBody UpdateFixtureRequest requestDto) {
         Fixture fixture = fixtureService.updateFixture(fixtureId, requestDto);
-        return ResponseEntity.status(HttpStatus.OK).body( fixtureService.convertFixtureToDto(fixture));
+        FixtureResponse responseDto = FixtureMapper.toResponse(fixture);
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
     @DeleteMapping("/{fixtureId}")
