9a6d3bf1a09efe8b89e7e195b588e69892c31261
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-11-28 12:35:44 +0200

    Remove the method to convert result objects to result response dtos

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/result/ResultServiceInterface.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/result/ResultServiceInterface.java
@@ -1,13 +1,11 @@
 package co.za.tlhalefosebaeng.leagueoracle.service.result;
 
 import co.za.tlhalefosebaeng.leagueoracle.dto.result.ResultRequest;
-import co.za.tlhalefosebaeng.leagueoracle.dto.result.ResultResponse;
 import co.za.tlhalefosebaeng.leagueoracle.model.Result;
 
 import java.util.List;
 
 public interface ResultServiceInterface {
-    ResultResponse convertResultToDto(Result result);
     Result addResult(Long fixtureId, ResultRequest requestDto);
     List<Result> getLeagueResults(Long leagueId);
     Result getResult(Long resultId);
