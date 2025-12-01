9005b2593267aeb8a529d4eaaa0a17517c465bd5
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-01 17:58:10 +0200

    Extend the once per request filter

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/filters/RequestResponseLoggerFilter.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/filters/RequestResponseLoggerFilter.java
@@ -1,4 +1,17 @@
 package co.za.tlhalefosebaeng.leagueoracle.filters;
 
-public class RequestResponseLoggerFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class RequestResponseLoggerFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
 }
