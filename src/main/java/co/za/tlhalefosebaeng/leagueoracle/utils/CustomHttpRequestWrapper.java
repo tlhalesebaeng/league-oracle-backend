534abf3495f96d195fdea67e9e8217ded80b7aa8
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-01 20:50:21 +0200

    Create the constructor, override the getHeader method and add a setHeader method

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/utils/CustomHttpRequestWrapper.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/utils/CustomHttpRequestWrapper.java
@@ -7,5 +7,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CustomHttpRequestWrapper extends HttpServletRequestWrapper {
+    private final Map<String, String> map = new HashMap<>();
 
+    public CustomHttpRequestWrapper(HttpServletRequest request) {
+        super(request);
+    }
+
+    public void setHeader(String name, String value) {
+        map.put(name, value);
+    }
+
+    @Override
+    public String getHeader(String name) {
+        String header = map.get(name);
+        if(header != null) return header;
+        return super.getHeader(name);
+    }
 }
