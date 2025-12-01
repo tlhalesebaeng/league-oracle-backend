abb472b453d77fd709797812e8328dc27b77a718
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-01 17:59:39 +0200

    Make the filter a spring bean

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/filters/RequestResponseLoggerFilter.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/filters/RequestResponseLoggerFilter.java
@@ -6,10 +6,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 public class RequestResponseLoggerFilter extends OncePerRequestFilter {
     private static final Logger httpLogger = LoggerFactory.getLogger(RequestResponseLoggerFilter.class);
 
