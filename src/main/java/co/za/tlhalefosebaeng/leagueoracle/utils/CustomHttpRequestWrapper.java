16cc95654161a4f67b1afb73b276b4d6a109132f
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-01 20:49:25 +0200

    Extend the http servlet request wrapper

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/utils/CustomHttpRequestWrapper.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/utils/CustomHttpRequestWrapper.java
@@ -1,4 +1,11 @@
 package co.za.tlhalefosebaeng.leagueoracle.utils;
 
-public class CustomHttpRequestWrapper {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CustomHttpRequestWrapper extends HttpServletRequestWrapper {
+
 }
