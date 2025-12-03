1ba16ef63ae29ede8ac4f1b014b3cbe9bc9ff327
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-03 11:39:11 +0200

    Log audits to the audit logger file

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/controller/AuthController.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/controller/AuthController.java
@@ -12,6 +12,8 @@ import co.za.tlhalefosebaeng.leagueoracle.service.cookie.CookieService;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,39 +22,57 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("${api.endpoint.prefix}/auth")
 public class AuthController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
     private final UserServiceInterface userService;
     private final JwtServiceInterface jwtService;
     private final CookieService cookieService;
 
     @PostMapping("/signup")
-    public ResponseEntity<AuthResponse> signup(@Valid @RequestBody SignupRequest requestDto, HttpServletResponse response){
+    public ResponseEntity<AuthResponse> signup(@RequestAttribute("correlation-id") String correlationId, @Valid @RequestBody SignupRequest requestDto, HttpServletResponse response){
+        LOGGER.info("Attempting Signup: {}", correlationId);
+
         User user = userService.addUser(requestDto);
         String jwt = jwtService.generateToken(user);
         response.addCookie(cookieService.create("access_jwt", jwt));
         UserResponse responseDto = UserMapper.toResponse(user);
+
+        LOGGER.info("Signup Successful: {} UserId {}", correlationId, responseDto.getId());
         return ResponseEntity.status(HttpStatus.CREATED).body(new AuthResponse(true, responseDto));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest requestDto, HttpServletResponse response) {
+    public ResponseEntity<AuthResponse> login(@RequestAttribute("correlation-id") String correlationId, @Valid @RequestBody LoginRequest requestDto, HttpServletResponse response) {
+        LOGGER.info("Attempting Login: {}", correlationId);
+
         User user = userService.login(requestDto);
         String jwt = jwtService.generateToken(user);
         response.addCookie(cookieService.create("access_jwt", jwt));
         UserResponse responseDto = UserMapper.toResponse(user);
+
+        LOGGER.info("Login Successful: {} UserId {}", correlationId, responseDto.getId());
         return ResponseEntity.status(HttpStatus.OK).body(new AuthResponse(true, responseDto));
     }
 
     @GetMapping("/check")
-    public ResponseEntity<AuthResponse> checkAuth(@CookieValue("access_jwt") String jwt) {
+    public ResponseEntity<AuthResponse> checkAuth(@RequestAttribute("correlation-id") String correlationId, @CookieValue("access_jwt") String jwt) {
+        LOGGER.info("Attempting Auth Check: {}", correlationId);
         User user = userService.checkAuth(jwt);
-        if(user == null) return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new AuthResponse(false, null));
+
+        if(user == null) {
+            LOGGER.info("Auth Check Successful, Not Authenticated: {}", correlationId);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new AuthResponse(false, null));
+        }
+
         UserResponse responseDto = UserMapper.toResponse(user);
+        LOGGER.info("Auth Check Successful, Authenticated: {} UserId {}", correlationId, responseDto.getId());
         return ResponseEntity.status(HttpStatus.OK).body(new AuthResponse(true, responseDto));
     }
 
     @GetMapping("/logout")
-    public ResponseEntity<Object> logout(HttpServletResponse response) {
+    public ResponseEntity<Object> logout(@RequestAttribute("correlation-id") String correlationId, HttpServletResponse response) {
+        LOGGER.info("Attempting Logout: {}", correlationId);
         response.addCookie(cookieService.create("access_jwt", ""));
+        LOGGER.info("Logout Successful: {}", correlationId);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null); // No need to return anything to the client
     }
 }
