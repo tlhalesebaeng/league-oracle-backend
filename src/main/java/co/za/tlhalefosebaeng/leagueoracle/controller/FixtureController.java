13335e6b5bc8ea972ef2a824ebeef23fa39c7477
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-04 11:11:54 +0200

    Audit controller user actions

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/controller/FixtureController.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/controller/FixtureController.java
@@ -7,6 +7,8 @@ import co.za.tlhalefosebaeng.leagueoracle.entity.Fixture;
 import co.za.tlhalefosebaeng.leagueoracle.service.fixture.FixtureServiceInterface;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,46 +19,59 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("${api.endpoint.prefix}/fixtures")
 public class FixtureController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FixtureController.class);
     private final FixtureServiceInterface fixtureService;
 
     @PostMapping("")
-    public ResponseEntity<List<FixtureResponse>> createLeagueFixtures(@RequestParam Long leagueId) {
+    public ResponseEntity<List<FixtureResponse>> createLeagueFixtures(@RequestAttribute("correlation-id") String correlationId, @RequestParam Long leagueId) {
+        LOGGER.info("Attempting Create League Fixtures: {} LeagueId {}", correlationId, leagueId);
         List<Fixture> fixtures = fixtureService.generateLeagueFixtures(leagueId);
         List<FixtureResponse> responseDto = FixtureMapper.toResponseList(fixtures);
+        LOGGER.info("Create League Fixtures Successful: {} LeagueId {} Fixtures {}", correlationId, leagueId, responseDto.size());
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
     }
 
     @GetMapping("")
-    public ResponseEntity<List<FixtureResponse>> getAllLeagueFixtures(@RequestParam Long leagueId) {
+    public ResponseEntity<List<FixtureResponse>> getAllLeagueFixtures(@RequestAttribute("correlation-id") String correlationId, @RequestParam Long leagueId) {
+        LOGGER.info("Attempting Get All League Fixtures: {} LeagueId {}", correlationId, leagueId);
         List<Fixture> fixtures = fixtureService.getAllLeagueFixtures(leagueId);
         List<FixtureResponse> responseDto = FixtureMapper.toResponseList(fixtures);
+        LOGGER.info("Get All League Fixtures Successful: {} LeagueId {} Fixtures {}", correlationId, leagueId, responseDto.size());
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
     @GetMapping("/{fixtureId}")
-    public ResponseEntity<FixtureResponse> getFixture(@PathVariable Long fixtureId) {
+    public ResponseEntity<FixtureResponse> getFixture(@RequestAttribute("correlation-id") String correlationId, @PathVariable Long fixtureId) {
+        LOGGER.info("Attempting Get Fixture: {} FixtureId {}", correlationId, fixtureId);
         Fixture fixture = fixtureService.getFixture(fixtureId);
         FixtureResponse responseDto = FixtureMapper.toResponse(fixture);
+        LOGGER.info("Get Fixture Successful: {} FixtureId {}", correlationId, responseDto.getId()); // Use response id to confirm that the ids are similar
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
     @GetMapping("/upcoming")
-    public ResponseEntity<List<FixtureResponse>> getUpcomingFixtures(@RequestParam Integer month) {
+    public ResponseEntity<List<FixtureResponse>> getUpcomingFixtures(@RequestAttribute("correlation-id") String correlationId, @RequestParam Integer month) {
+        LOGGER.info("Attempting Get Upcoming Fixtures: {} Month {}", correlationId, month);
         List<Fixture> fixtures = fixtureService.getUpcomingFixtures(month);
         List<FixtureResponse> responseDto = FixtureMapper.toResponseList(fixtures);
+        LOGGER.info("Get Upcoming Fixtures Successful: {} Month {} Fixtures {}", correlationId, month, responseDto.size());
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
     @PatchMapping("/{fixtureId}")
-    public ResponseEntity<FixtureResponse> updateFixture(@PathVariable Long fixtureId, @Valid @RequestBody UpdateFixtureRequest requestDto) {
+    public ResponseEntity<FixtureResponse> updateFixture(@RequestAttribute("correlation-id") String correlationId, @PathVariable Long fixtureId, @Valid @RequestBody UpdateFixtureRequest requestDto) {
+        LOGGER.info("Attempting Update Fixture: {} FixtureId {}", correlationId, fixtureId);
         Fixture fixture = fixtureService.updateFixture(fixtureId, requestDto);
         FixtureResponse responseDto = FixtureMapper.toResponse(fixture);
+        LOGGER.info("Update Fixture Successful: {} FixtureId {}", correlationId, responseDto.getId()); // Use response id to confirm that the ids are similar
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
     @DeleteMapping("/{fixtureId}")
-    public ResponseEntity<Object> deleteFixture(@PathVariable Long fixtureId) {
+    public ResponseEntity<Object> deleteFixture(@RequestAttribute("correlation-id") String correlationId, @PathVariable Long fixtureId) {
+        LOGGER.info("Attempting Delete Fixture: {} FixtureId {}", correlationId, fixtureId);
         fixtureService.deleteFixture(fixtureId);
+        LOGGER.info("Delete Fixture Successful: {}", correlationId);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
     }
 }
