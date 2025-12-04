d7b3443b3faf70f1a74594f8c6bc530ec6084676
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-04 12:32:31 +0200

    Log user actions

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/controller/TeamController.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/controller/TeamController.java
@@ -8,6 +8,8 @@ import co.za.tlhalefosebaeng.leagueoracle.mapper.TeamMapper;
 import co.za.tlhalefosebaeng.leagueoracle.service.team.TeamServiceInterface;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,39 +20,50 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("${api.endpoint.prefix}/teams")
 public class TeamController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TeamController.class);
     private final TeamServiceInterface teamService;
 
     @PostMapping("")
-    public ResponseEntity<TeamResponse> addTeam(@RequestParam Long leagueId, @Valid @RequestBody AddTeamRequest requestDto){
+    public ResponseEntity<TeamResponse> addTeam(@RequestAttribute("correlation-id") String correlationId, @RequestParam Long leagueId, @Valid @RequestBody AddTeamRequest requestDto){
+        LOGGER.info("Attempting Add Team: {} LeagueId {}", correlationId, leagueId);
         Team team = teamService.addTeam(leagueId, requestDto);
         TeamResponse responseDto = TeamMapper.toResponse(team);
+        LOGGER.info("Add Team Successful: {} TeamId {}", correlationId, responseDto.getId());
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
     @GetMapping("")
-    public ResponseEntity<List<TeamResponse>> getAllLeagueTeams(@RequestParam Long leagueId) {
+    public ResponseEntity<List<TeamResponse>> getAllLeagueTeams(@RequestAttribute("correlation-id") String correlationId, @RequestParam Long leagueId) {
+        LOGGER.info("Attempting Get All League Teams: {} LeagueId {}", correlationId, leagueId);
         List<Team> teams = teamService.getAllLeagueTeams(leagueId);
         List<TeamResponse> responseDto = TeamMapper.toResponseList(teams);
+        LOGGER.info("Get All League Teams Successful: {} Leagues {}", correlationId, responseDto.size());
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
     @GetMapping("/{teamId}")
-    public ResponseEntity<TeamResponse> getTeam(@PathVariable Long teamId) {
+    public ResponseEntity<TeamResponse> getTeam(@RequestAttribute("correlation-id") String correlationId, @PathVariable Long teamId) {
+        LOGGER.info("Attempting Get Team: {} TeamId {}", correlationId, teamId);
         Team team = teamService.getTeam(teamId);
         TeamResponse responseDto = TeamMapper.toResponse(team);
+        LOGGER.info("Get Team Successful: {} TeamId {}", correlationId, responseDto.getId()); // Log response id to confirm that the ids are similar
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
     @PatchMapping("/{teamId}")
-    public ResponseEntity<TeamResponse> updateTeam(@RequestParam Long leagueId, @PathVariable Long teamId, @Valid @RequestBody UpdateTeamRequest requestDto) {
+    public ResponseEntity<TeamResponse> updateTeam(@RequestAttribute("correlation-id") String correlationId, @PathVariable Long teamId, @Valid @RequestBody UpdateTeamRequest requestDto) {
+        LOGGER.info("Attempting Update Team: {} TeamId {}", correlationId, teamId);
         Team team = teamService.updateTeam(teamId, requestDto);
         TeamResponse responseDto = TeamMapper.toResponse(team);
+        LOGGER.info("Update Team Successful: {} TeamId {}", correlationId, responseDto.getId()); // Log response id to confirm that the ids are similar
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
     @DeleteMapping("/{teamId}")
-    public ResponseEntity<Object> deleteTeam(@PathVariable Long teamId) {
+    public ResponseEntity<Object> deleteTeam(@RequestAttribute("correlation-id") String correlationId, @PathVariable Long teamId) {
+        LOGGER.info("Attempting Delete Team: {} TeamId {}", correlationId, teamId);
         teamService.deleteTeam(teamId);
+        LOGGER.info("Delete Team Successful: {}", correlationId);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
     }
 
