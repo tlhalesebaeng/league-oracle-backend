19e1a4897f1567d51f64cb1be5f147176def2289
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-05 13:48:54 +0200

    Log all the preparation and successful execution of tasks that the methods perform

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/team/TeamService.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/service/team/TeamService.java
@@ -7,7 +7,10 @@ import co.za.tlhalefosebaeng.leagueoracle.entity.League;
 import co.za.tlhalefosebaeng.leagueoracle.entity.Team;
 import co.za.tlhalefosebaeng.leagueoracle.repository.TeamRepository;
 import co.za.tlhalefosebaeng.leagueoracle.service.league.LeagueServiceInterface;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -17,11 +20,15 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class TeamService implements TeamServiceInterface{
+    private static final Logger LOGGER = LoggerFactory.getLogger(TeamService.class);
     private final LeagueServiceInterface leagueService;
     private final TeamRepository teamRepo;
+    private final HttpServletRequest request;
 
     @Override
     public Team addTeam(Long leagueId, AddTeamRequest requestDto) {
+        String correlationId = (String) request.getAttribute("correlation-id");
+        LOGGER.info("Preparing to add team: {} LeagueId {}", correlationId, leagueId);
         League league = leagueService.getLeague(leagueId);
 
         // Confirm the creator of this league - Only logged-in creator of league should be able to add teams of a league
@@ -45,19 +52,27 @@ public class TeamService implements TeamServiceInterface{
         team.setLeague(league);
         team.setName(requestDto.getName());
 
-        return teamRepo.save(team);
+        Team dbTeam = teamRepo.save(team);
+        LOGGER.info("Successfully added team: {} TeamId {}", correlationId, dbTeam.getId());
+        return dbTeam;
     }
 
     @Override
     public Team getTeam(Long teamId) {
-        Optional<Team> team = teamRepo.findById(teamId);
-        return team.orElseThrow(() -> new AppException(HttpStatus.BAD_REQUEST ,"Team not found! Please check team ID and try again"));
+        String correlationId = (String) request.getAttribute("correlation-id");
+        LOGGER.info("Preparing to fetch team: {} TeamId {}", correlationId, teamId);
+        Optional<Team> dbTeam = teamRepo.findById(teamId);
+        Team team = dbTeam.orElseThrow(() -> new AppException(HttpStatus.BAD_REQUEST, "Team not found! Please check team ID and try again"));
+        LOGGER.info("Successfully fetched team: {} TeamId {}", correlationId, team.getId()); // Log the team id to confirm that the ids are similar
+        return team;
     }
 
     @Override
     public Team updateTeam(Long teamId, UpdateTeamRequest requestDto) {
-        Team team = this.getTeam(teamId);
-        League league = leagueService.getLeague(team.getLeague().getId());
+        String correlationId = (String) request.getAttribute("correlation-id");
+        LOGGER.info("Preparing to update team: {} TeamId {}", correlationId, teamId);
+        Team dbTeam = this.getTeam(teamId);
+        League league = leagueService.getLeague(dbTeam.getLeague().getId());
 
         // Confirm the creator of this league - Only logged-in creator of league should be able to update teams of a league
         if(!leagueService.isCreator(league)) {
@@ -65,19 +80,27 @@ public class TeamService implements TeamServiceInterface{
         }
 
         // To increase the integrity of the system, only the team name can be updated. Other team fields should be updated through results
-        if(requestDto.getName() != null) team.setName(requestDto.getName());
+        if(requestDto.getName() != null) dbTeam.setName(requestDto.getName());
 
-        return teamRepo.save(team);
+        Team team = teamRepo.save(dbTeam);
+        LOGGER.info("Successfully updated team: {} TeamId {}", correlationId, team.getId()); // Log the team id to confirm that the ids are similar
+        return team;
     }
 
     @Override
     public List<Team> getAllLeagueTeams(Long leagueId) {
+        String correlationId = (String) request.getAttribute("correlation-id");
+        LOGGER.info("Preparing to fetch all league teams: {} LeagueId {}", correlationId, leagueId);
         League league = leagueService.getLeague(leagueId);
-        return teamRepo.findAllByLeagueId(league.getId());
+        List<Team> teams = teamRepo.findAllByLeagueId(league.getId());
+        LOGGER.info("Successfully fetched all league teams: {} LeagueId {} Teams {}", correlationId, leagueId, teams.size());
+        return teams;
     }
 
     @Override
     public void deleteTeam(Long teamId) {
+        String correlationId = (String) request.getAttribute("correlation-id");
+        LOGGER.info("Preparing to delete team: {} TeamId {}", correlationId, teamId);
         Team team = this.getTeam(teamId);
         League league = leagueService.getLeague(team.getLeague().getId());
 
@@ -87,5 +110,6 @@ public class TeamService implements TeamServiceInterface{
         }
 
         teamRepo.delete(team);
+        LOGGER.info("Successfully deleted team: {}", correlationId);
     }
 }
