dddf2c54f92f98a78f5ce9c8d94754ca919019cf
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-12-05 13:36:53 +0200

    Log whether the route is protected or not

--- src/main/java/co/za/tlhalefosebaeng/leagueoracle/filters/RequestsLoggerFilter.java
+++ src/main/java/co/za/tlhalefosebaeng/leagueoracle/filters/RequestsLoggerFilter.java
@@ -1,9 +1,11 @@
 package co.za.tlhalefosebaeng.leagueoracle.filters;
 
+import co.za.tlhalefosebaeng.leagueoracle.service.routes.RoutesService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -12,9 +14,11 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 import java.util.UUID;
 
+@AllArgsConstructor
 @Component
 public class RequestsLoggerFilter extends OncePerRequestFilter {
-    private static final Logger httpLogger = LoggerFactory.getLogger(RequestsLoggerFilter.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(RequestsLoggerFilter.class);
+    private final RoutesService routesService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -22,17 +26,19 @@ public class RequestsLoggerFilter extends OncePerRequestFilter {
         String uri = request.getRequestURI();
         String queryString = request.getQueryString();
         String query = queryString != null ? "?" + queryString : "";
+        boolean isProtected = routesService.isProtected(method, uri);
         String correlationId = UUID.randomUUID().toString();
+        String routeIsProtected = isProtected ? "Protected" : "Not Protected";
 
         request.setAttribute("correlation-id", correlationId);
 
-        httpLogger.info("Incoming: {} {} {}{}",correlationId, method, uri, query);
+        LOGGER.info("Incoming: {} {} {} {}{}", correlationId, routeIsProtected, method, uri, query);
 
         long start = System.currentTimeMillis();
         filterChain.doFilter(request, response);
         long total = System.currentTimeMillis() - start;
 
         int status = response.getStatus();
-        httpLogger.info("Outgoing: {} HTTP {} {} ms", correlationId, status, total);
+        LOGGER.info("Outgoing: {} HTTP {} {} ms", correlationId, status, total);
     }
 }
